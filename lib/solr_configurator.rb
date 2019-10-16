5a79edb906f2477075c5ab01956e2d6ee74f0800
Dann Bohn <djb44@psu.edu>
2019-10-16 11:31:14 -0400

    moves idempotency out of class, and into rake tasks

--- lib/solr_configurator.rb
+++ lib/solr_configurator.rb
@@ -3,57 +3,46 @@
 require 'zip'
 
 class SolrConfigurator
-  def initialize
-    connection
-  end
+  def initialize; end
 
   def zip_file
     File.open(tempfile)
   end
 
   def configset_exists?
-    resp = @connection.get('/solr/admin/configs?action=LIST&omitHeader=true')
+    resp = connection.get('/solr/admin/configs',
+                          action: 'LIST')
     JSON.parse(resp.body)['configSets'].include?(configset_name)
   end
 
   def collection_exists?
-    resp = @connection.get('/solr/admin/collections?action=LIST')
+    resp = connection.get('/solr/admin/collections',
+                          action: 'LIST')
     JSON.parse(resp.body)['collections'].include?(collection_name)
   end
 
   def create_collection
-    if collection_exists?
-      return true
-    end
-
-    resp = @connection.get("/solr/admin/collections?action=CREATE&name=#{collection_name}&numShards=#{num_shards}")
+    resp = connection.get('/solr/admin/collections',
+                          action: 'CREATE',
+                          name: collection_name,
+                          numShards: num_shards)
     check_resp(resp)
   end
 
   def modify_collection
-    puts 'Modifying Collection to use Configset'
-    uri = %W(
-      /solr/admin/collections
-      ?action=MODIFYCOLLECTION
-      &collection=#{collection_name}
-      &collection.configName=#{@configset_name}
-    ).join
-    resp = @connection.get(uri)
+    resp = connection.get('/solr/admin/collections',
+                          action: 'MODIFYCOLLECTION',
+                          collection: collection_name,
+                          "collection.configName": configset_name)
     check_resp(resp)
   end
 
   def upload_config
-    if configset_exists?
-      puts "Configset #{configset_name} already exists. Skipping Upload"
-      return true
-    end
-    puts "Uploading #{configset_name}"
-    resp = @connection.post do |req|
-      req.url "/solr/admin/configs?action=UPLOAD&name=#{configset_name}"
+    resp = connection.post('/solr/admin/configs') do |req|
+      req.params = { "action": 'UPLOAD', "name": configset_name }
       req.headers['Content-Type'] = 'octect/stream'
       req.body = raw_data
     end
-    puts 'Finished uploading...'
     check_resp(resp)
   end
 
@@ -78,19 +67,12 @@ class SolrConfigurator
     end
 
     # Gets a response object, if it's status code is not 200, we emit the body and bail
-    # This can be better! PRs accepted.
     def check_resp(resp)
-      if resp.status == 200
-        true
-      else
-        puts resp.body
-        exit(1) # rubocop:disable Rails/Exit
-      end
+      raise resp.body unless resp.status == 200
     end
 
     def tempfile
       tmp = Tempfile.new('configset')
-      puts "Zipping up files in #{solr_config_dir}"
       Zip::File.open(tmp, Zip::File::CREATE) do |zipfile|
         Dir["#{solr_config_dir}/**/**"].each do |file|
           zipfile.add(file.sub("#{solr_config_dir}/", ''), file)
@@ -102,7 +84,6 @@ class SolrConfigurator
     def connection
       @connection ||= Faraday.new("http://#{solr_host}:#{solr_port}") do |faraday|
         faraday.request :multipart
-        # faraday.response :logger
         faraday.adapter :net_http
       end
     end
