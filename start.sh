baa0188d9227448dbb8c493600ded5fea3d45d1d
Dann <whereismyjetpack@users.noreply.github.com>
2019-11-05 09:49:17 -0500

    Docker fixes (#49)
    Changes:
    * optimize some of the docker patterns
    * adds missing aws configurations, don't need interpolation in S3_ENDPOINT
    * remove pid file if it already exists

--- start.sh
+++ start.sh
@@ -5,6 +5,7 @@ if [ ${APP_ROLE:-app} == "sidekiq" ]; then
     bundle exec sidekiq
 else
     echo "starting rails"
+    rm -f tmp/pids/server.pid
     bundle exec rails db:create
     bundle exec rails db:migrate
     bundle exec rails solr:init
