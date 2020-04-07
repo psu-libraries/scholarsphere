3aedb309002963e1b0b5a5776c861c788c6317ff
Adam Wead <awead@users.noreply.github.com>
2020-04-07 16:33:58 -0400

    Fix Blacklight's search API
    Returning search results in json was broken because we've moved away
    from SolrDocument and we weren't testing the integration endpoint.
    
    We're now testing json responses from the CatalogController and
    overriding the jbuilder file to make it work for now, but there's a
    better solution down the road.

--- spec/features/discovery_search_spec.rb
+++ spec/features/discovery_search_spec.rb
@@ -33,4 +33,19 @@ RSpec.describe 'Searching discoverable resources' do
       expect(page).to have_content(authorized_work.latest_published_version.title)
     end
   end
+
+  context 'when searching via the Blacklight API' do
+    let(:results) { JSON.parse(page.body) }
+
+    it 'returns search results in json' do
+      visit search_catalog_path(format: :json)
+
+      expect(
+        results['data'].map { |record| record['id'] }
+      ).to contain_exactly(
+        public_work.uuid,
+        previous_embargoed_work.uuid
+      )
+    end
+  end
 end
