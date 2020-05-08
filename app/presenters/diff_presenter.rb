da858859053b040af2512059c055e87762d8c900
Adam Wead <awead@users.noreply.github.com>
2020-05-08 15:18:27 -0400

    Use OpenStruct instead of Struct
    Our own RenamedFile#size method overrides the Struct#size method, so the
    easiest way to avoid this problem is to use OpenStruct which doesn't
    have its own #size method, it's just invoked with a hash instead of
    positional arguments.

--- app/presenters/diff_presenter.rb
+++ app/presenters/diff_presenter.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 class DiffPresenter
-  # @note Struct representing a renamed file. The title is a Diffy::Diff so we can show what parts of the titile
+  # @note OpenStruct representing the renamed file. The title is a Diffy::Diff so we can show what parts of the title
   # changed, but it also includes the size and mime type so we can display it like any other file.
-  RenamedFile = Struct.new(:title, :size, :mime_type)
+  class RenamedFile < OpenStruct; end
 
   attr_reader :files,
               :creators
@@ -35,9 +35,9 @@ class DiffPresenter
   def renamed_files
     files.fetch(:renamed, []).map do |renamed_files|
       RenamedFile.new(
-        Diffy::Diff.new(*renamed_files.map(&:title)),
-        renamed_files.first.size,
-        renamed_files.first.mime_type
+        title: Diffy::Diff.new(*renamed_files.map(&:title)),
+        size: renamed_files.first.size,
+        mime_type: renamed_files.first.mime_type
       )
     end
   end
