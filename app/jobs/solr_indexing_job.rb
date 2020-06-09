bc7c50e783cb60b00c8c7ed9b0d57766d5d8e268
Ryan Schenk <rschenk@gmail.com>
2020-06-09 14:55:22 -0400

    Queue indexing jobs in indexing queue

--- app/jobs/solr_indexing_job.rb
+++ app/jobs/solr_indexing_job.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class SolrIndexingJob < ApplicationJob
-  queue_as :default
+  queue_as :indexing
 
   def perform(indexable_resource)
     indexable_resource.update_index
