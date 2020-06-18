8da15dab82b2b7e033c5e52d201b86a816633cad
Adam Wead <awead@users.noreply.github.com>
2020-06-18 12:59:40 -0400

    Set defaults for url host and protocol
    Instead of requiring the host, we can have defaults for local
    development and set the protocol as well to construct the full url when
    needed.

--- config/initializers/default_url_host.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-default_url_host = ENV['DEFAULT_URL_HOST']
-Rails.application.routes.default_url_options[:host] = default_url_host
-
-if default_url_host.blank?
-  raise "ENV['DEFAULT_URL_HOST'] is required to be set"
-end
