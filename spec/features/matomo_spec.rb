f49b7c731e532ad24d485dce9a573d25a694c041
Adam Wead <awead@users.noreply.github.com>
2020-06-25 10:51:33 -0400

    Install Matomo tracking script
    Uses our Matomo application to capture statistics for page views and
    other interactions.
    
    Co-authored-by: Dann Bohn <djb44@psu.edu>

---
+++ spec/features/matomo_spec.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'Matomo Traking Code' do
+  before { Capybara.ignore_hidden_elements = false }
+
+  after { Capybara.ignore_hidden_elements = true }
+
+  it 'has the default configuration' do
+    visit(blacklight_path)
+    expect(page).to have_content('https://analytics.libraries.psu.edu/matomo')
+    expect(page).to have_content('15')
+    expect(page).to have_content('/* tracker methods like "setCustomDimension" should be called before "trackPageView" */')
+  end
+end
