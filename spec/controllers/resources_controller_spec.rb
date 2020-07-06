bf47d32fd24ccb19e6c342534de9f9da02d2e850
Adam Wead <awead@users.noreply.github.com>
2020-07-06 15:42:58 -0400

    Log only unique views to pages
    Stores a key using the work's uuid and the current session id that only
    increments unique visits to a resource page.

--- spec/controllers/resources_controller_spec.rb
+++ spec/controllers/resources_controller_spec.rb
@@ -4,6 +4,8 @@ require 'rails_helper'
 
 RSpec.describe ResourcesController, type: :controller do
   describe '#show' do
+    let(:redis) { Redis.new(Rails.configuration.redis) }
+
     context 'when requesting a Work' do
       let(:work) { create(:work, has_draft: false) }
 
@@ -18,6 +20,30 @@ RSpec.describe ResourcesController, type: :controller do
         }.to change {
           ViewStatistic.where(resource_type: 'WorkVersion', resource_id: work.latest_published_version).count
         }.from(0).to(1)
+      end
+    end
+
+    context 'when requesting a work for the first time in a session' do
+      let(:work) { create(:work, has_draft: false) }
+
+      it 'marks it as a unique view' do
+        expect {
+          get :show, params: { id: work.uuid }
+        }.to change {
+          redis.keys.count
+        }.by(1)
+      end
+    end
+
+    context 'when requesting the same work twice the same session' do
+      let(:work) { create(:work, has_draft: false) }
+
+      before { get :show, params: { id: work.uuid } }
+
+      it 'does NOT mark the request in the cache' do
+        expect {
+          get :show, params: { id: work.uuid }
+        }.not_to(change { redis.keys.count })
       end
     end
 
