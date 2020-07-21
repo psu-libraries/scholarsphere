bd1a2e1ee937ce571b5489b3c307431fe65addef
Dann Bohn <djb44@psu.edu>
2020-07-21 14:44:24 -0400

    look into json logging

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -2,6 +2,7 @@
 
 Sidekiq.configure_server do |config|
   config.redis = Rails.configuration.redis
+  config.log_formatter = Sidekiq::Logger::Formatters::JSON.new
 end
 
 Sidekiq.configure_client do |config|
