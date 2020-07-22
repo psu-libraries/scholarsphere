2ebd162567d78f8258f341b1fbfcdbd264a0f350
Adam Wead <awead@users.noreply.github.com>
2020-07-22 13:39:22 -0400

    Remove sass-rails and sprockets
    Since we're using webpacker for everything sass and js related, we don't
    need sprockets anymore.

--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,6 @@ require 'action_mailbox/engine'
 require 'action_text/engine'
 require 'action_view/railtie'
 require 'action_cable/engine'
-require 'sprockets/railtie'
 require 'view_component/engine'
 
 # Require the gems listed in Gemfile, including any gems
