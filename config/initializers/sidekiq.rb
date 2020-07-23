b42b4dc3ad4dcae2b25fea8d35bd55cf15a8d2a1
Dann <whereismyjetpack@users.noreply.github.com>
2020-07-23 09:20:41 -0400

    Preview/sidekiq log (#423)
    Log json for sidekiq client and server in production.

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -2,8 +2,10 @@
 
 Sidekiq.configure_server do |config|
   config.redis = Rails.configuration.redis
+  config.log_formatter = Sidekiq::Logger::Formatters::JSON.new if Rails.env.production?
 end
 
 Sidekiq.configure_client do |config|
   config.redis = Rails.configuration.redis
+  config.log_formatter = Sidekiq::Logger::Formatters::JSON.new if Rails.env.production?
 end
