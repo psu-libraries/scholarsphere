b653913adbcc66918156d6689e048bf464d68377
Ryan Schenk <rschenk@gmail.com>
2020-08-11 13:48:46 -0400

    Update analytics chart for new frontend design

--- app/javascript/frontend/index.js
+++ app/javascript/frontend/index.js
@@ -5,6 +5,9 @@ import './styles'
 require('jquery/dist/jquery')
 require('bootstrap/dist/js/bootstrap')
 
+// View statistics
+import './scripts/view_statistics_chart'
+
 // Load images
 // Retrieve the path to the image via <%= image_pack_tag('image.png') %>
 require.context('./img/', true)
