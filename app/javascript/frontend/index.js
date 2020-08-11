f8156038032816d5e44611270b5296ffde71513b
Ryan Schenk <rschenk@gmail.com>
2020-08-11 14:02:34 -0400

    Make eslint happy

--- app/javascript/frontend/index.js
+++ app/javascript/frontend/index.js
@@ -2,11 +2,11 @@
 // Load styles
 import './styles'
 
-require('jquery/dist/jquery')
-require('bootstrap/dist/js/bootstrap')
-
 // View statistics
 import './scripts/view_statistics_chart'
+
+require('jquery/dist/jquery')
+require('bootstrap/dist/js/bootstrap')
 
 // Load images
 // Retrieve the path to the image via <%= image_pack_tag('image.png') %>
