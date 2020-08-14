8719bd1686f520a650c300cbe96f2084ba37874f
Dann Bohn <djb44@psu.edu>
2020-08-14 11:24:42 -0400

    optimize query for mailer

--- app/jobs/mailers/monthly_stats.rb
+++ app/jobs/mailers/monthly_stats.rb
@@ -5,7 +5,7 @@ module Mailers
     queue_as :mailers
 
     def perform
-      User.where(provider: 'azure_oauth', opt_out_stats_email: false, active: true).each do |user|
+      User.includes(:actor).where(provider: 'azure_oauth', opt_out_stats_email: false, active: true).find_each do |user|
         ActorMailer.with(actor: user.actor).monthly_stats.deliver_later
       end
     end
