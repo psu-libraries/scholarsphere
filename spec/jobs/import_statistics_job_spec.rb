7b45c0000bcaf465bedf556793bdd5654cafea7b
Adam Wead <awead@users.noreply.github.com>
2020-10-28 11:22:10 -0400

    Migrate statistics via ImportStatisticsJob
    Migrating statistics from Scholarsphere 3 is a time-intensive task and
    will need to be run repeatedly. This extracts our current rake task into
    multiple jobs which can be run asynchronously via Sidekiq.

---
+++ spec/jobs/import_statistics_job_spec.rb
@@ -0,0 +1,45 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe ImportStatisticsJob, type: :job do
+  describe '#perform' do
+    def import_from_csv
+      csv = Pathname.new(fixture_path).join('s3_export_stats.csv')
+
+      CSV.foreach(csv) do |row|
+        described_class.perform_now(row)
+      end
+    end
+
+    context 'when the work is published' do
+      before { create(:legacy_identifier, old_id: 'tb09j581r', resource: create(:work, has_draft: false)) }
+
+      it 'imports view statistics from Scholarsphere 3' do
+        expect {
+          import_from_csv
+        }.to change(ViewStatistic, :count).by(4)
+      end
+    end
+
+    context 'when the work is not published' do
+      before { create(:legacy_identifier, old_id: 'tb09j581r', resource: create(:work, has_draft: true)) }
+
+      it 'imports view statistics from Scholarsphere 3' do
+        expect {
+          import_from_csv
+        }.not_to change(ViewStatistic, :count)
+      end
+    end
+
+    context 'with files' do
+      before { create(:legacy_identifier, :with_file, old_id: 'rxw42n8719') }
+
+      it 'imports view statistics from Scholarsphere 3' do
+        expect {
+          import_from_csv
+        }.to change(ViewStatistic, :count).by(3)
+      end
+    end
+  end
+end
--- spec/lib/tasks/migration_spec.rb
+++ spec/lib/tasks/migration_spec.rb
@@ -3,39 +3,14 @@
 require 'rails_helper'
 
 describe 'migration' do
-  after { Rake::Task['migration:statistics'].reenable }
-
   describe ':statistics' do
     let(:csv) { Pathname.new(fixture_path).join('s3_export_stats.csv') }
 
-    context 'when the work is published' do
-      before { create(:legacy_identifier, old_id: 'tb09j581r', resource: create(:work, has_draft: false)) }
+    before { allow(ImportStatisticsJob).to receive(:perform_later) }
 
-      it 'imports view statistics from Scholarsphere 3' do
-        expect {
-          Rake::Task['migration:statistics'].invoke(csv)
-        }.to change(ViewStatistic, :count).by(4)
-      end
-    end
-
-    context 'when the work is not published' do
-      before { create(:legacy_identifier, old_id: 'tb09j581r', resource: create(:work, has_draft: true)) }
-
-      it 'imports view statistics from Scholarsphere 3' do
-        expect {
-          Rake::Task['migration:statistics'].invoke(csv)
-        }.not_to change(ViewStatistic, :count)
-      end
-    end
-
-    context 'with files' do
-      before { create(:legacy_identifier, :with_file, old_id: 'rxw42n8719') }
-
-      it 'imports view statistics from Scholarsphere 3' do
-        expect {
-          Rake::Task['migration:statistics'].invoke(csv)
-        }.to change(ViewStatistic, :count).by(3)
-      end
+    it 'calls the ImportStatisticsJob' do
+      Rake::Task['migration:statistics'].invoke(csv)
+      expect(ImportStatisticsJob).to have_received(:perform_later).exactly(8).times
     end
   end
 end
