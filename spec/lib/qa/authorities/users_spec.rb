fefe7e50c167373d431c4b5451ae185bb8ea9047
Adam Wead <awead@users.noreply.github.com>
2020-10-29 12:40:19 -0400

    Define /authorities/search/users endpoint
    Uses Penn State's identity service to search for users that can be
    granted edit access.

---
+++ spec/lib/qa/authorities/users_spec.rb
@@ -0,0 +1,38 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Qa::Authorities::Users, type: :authority do
+  let(:authority) { described_class.new }
+  let(:mock_client) { instance_spy('PennState::SearchService::Client') }
+
+  describe '#search' do
+    subject(:results) { authority.search(search_term) }
+
+    before do
+      allow(PennState::SearchService::Client).to receive(:new).and_return(mock_client)
+      allow(mock_client).to receive(:search).and_return(mock_identity_response)
+    end
+
+    let(:formatted_result) do
+      {
+        given_name: person.given_name,
+        surname: person.surname,
+        psu_id: person.user_id,
+        default_alias: person.display_name,
+        email: person.university_email,
+        orcid: '',
+        source: 'penn state',
+        result_number: 1,
+        total_results: 1,
+        additional_metadata: "#{Actor.human_attribute_name(:psu_id)}: #{person.user_id}"
+      }
+    end
+
+    let(:mock_identity_response) { [person] }
+    let(:person) { build(:person) }
+    let(:search_term) { 'search query' }
+
+    it { is_expected.to include(formatted_result) }
+  end
+end
