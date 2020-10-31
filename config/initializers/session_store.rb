485584dcc49360cb53fb6d176d091aa23f4bd142
Dann Bohn <djb44@psu.edu>
2020-10-30 23:04:13 -0400

    moved session_store config to environments

--- config/initializers/session_store.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-Rails.application.config.session_store :cookie_store,
-                                       key: '_scholarsphere',
-                                       secure: Rails.env.production?,
-                                       httponly: Rails.env.production?
