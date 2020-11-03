59804ec4dbd3a8f2b07e0e36ff8e4622db5ddcab
Seth Erickson <sr.erickson@gmail.com>
2020-11-03 11:05:05 -0500

    fix navbar headings, help page route

--- config/routes.rb
+++ config/routes.rb
@@ -22,6 +22,7 @@ Rails.application.routes.draw do
 
   root to: 'pages#home'
   get 'about', to: 'markdown#show', page: 'about'
+  get 'help', to: 'markdown#show', page: 'help'
   get 'policies-1.0', to: 'markdown#show', page: 'policies-1.0'
   get 'policies-2.0', to: 'markdown#show', page: 'policies-2.0'
   get 'policies', to: 'markdown#show', page: 'policies-2.0'
