2d2407b1609336035c745e2e655ac39df1077e86
Ryan Schenk <rschenk@gmail.com>
2020-11-12 12:57:14 -0500

    Re-order creators on the Contributors form

--- app/javascript/controllers/contributors_controller.js
+++ app/javascript/controllers/contributors_controller.js
@@ -4,22 +4,17 @@ import { csrfToken } from './stimulus_modules'
 
 export default class extends Controller {
   connect () {
+    this.wrapperClass = this.data.get('wrapper-class')
+    this.badgeClass = this.data.get('badge-class')
+    this.positionClass = this.data.get('position-class')
+
     document.addEventListener('autocomplete:alternative', () => this.openModal(this.data.get('new')))
     document.addEventListener('autocomplete:after-selected', () => this.post(this.data.get('post')))
     document.addEventListener('actor:created', () => this.appendResponse(event.detail.response))
 
-    this.badgeClass = this.data.get('badge-class')
+    $(this.element).on('cocoon:after-remove', () => this.$updateChildren())
 
-    // @todo This uses jQuery. We might want to put these in a separate location for better organization
-    this.renumberBadges = () => {
-      $(this.element)
-        .find(`.${this.badgeClass}:visible`)
-        .each((index, item) => {
-          $(item).text(index + 1)
-        })
-    }
-
-    $(this.element).on('cocoon:after-remove', () => this.renumberBadges())
+    this.$updateChildren()
   }
 
   post (url) {
@@ -37,7 +32,7 @@ export default class extends Controller {
 
   appendResponse (response) {
     this.element.insertAdjacentHTML('beforeend', response.data)
-    this.renumberBadges()
+    this.$updateChildren()
   }
 
   // @todo Something nicer should go here.
@@ -53,5 +48,52 @@ export default class extends Controller {
       .catch(function (error) {
         console.log(error)
       })
+  }
+
+  moveUp (event) {
+    const $target = $(event.target)
+    const $parentWrapper = $target.closest(`.${this.wrapperClass}`)
+    const $previousSibling = $parentWrapper.prevAll(`.${this.wrapperClass}:visible`).first()
+
+    if ($previousSibling.length > 0) {
+      this._$swapElements($parentWrapper, $previousSibling)
+      this.$updateChildren()
+    }
+  }
+
+  moveDown (event) {
+    const $target = $(event.target)
+    const $parentWrapper = $target.closest(`.${this.wrapperClass}`)
+    const $nextSibling = $parentWrapper.nextAll(`.${this.wrapperClass}:visible`).first()
+
+    if ($nextSibling.length > 0) {
+      this._$swapElements($parentWrapper, $nextSibling)
+      this.$updateChildren()
+    }
+  }
+
+  // Uses jQuery
+  $updateChildren () {
+    const children = $(this.element)
+      .find(`.${this.wrapperClass}:visible`)
+
+    children
+      .each((index, wrapperElement) => {
+        $(wrapperElement).find(`.${this.badgeClass}`).text(index + 1)
+        $(wrapperElement).find(`.${this.positionClass}`).val((index + 1) * 10)
+        $(wrapperElement).find('.js-move-up').toggleClass('disabled', index === 0)
+        $(wrapperElement).find('.js-move-down').toggleClass('disabled', index === children.length - 1)
+      })
+  }
+
+  // Uses jQuery
+  _$swapElements ($el1, $el2) {
+    // create temporary placeholder
+    const $temp = $('<div>')
+
+    // 3-step swap
+    $el1.before($temp)
+    $el2.before($el1)
+    $temp.after($el2).remove()
   }
 }
