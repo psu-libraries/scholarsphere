c55b31445594c7085a5b04cf6b585473a4a03587
Adam Wead <awead@users.noreply.github.com>
2020-12-11 11:11:29 -0500

    Respond with json for errors in non-html formats
    Custom error responses need to account for the format of the original
    request. If a user requests a resource in a format other than html, such
    as xml, rss, or other, the error response will attempt the respond using
    the requested format. This was causing missing template errors because
    the application was trying to honor the request with the requested
    format.
    
    Since we don't want to attempt to match every kind of non-html response,
    we'll just respond with json for any non-html request that generates an
    error.

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -2,10 +2,16 @@
 
 class ErrorsController < ApplicationController
   def not_found
-    render 'not_found', status: :not_found
+    respond_to do |format|
+      format.html { render 'not_found', status: :not_found }
+      format.any { render json: { message: 'Record not found' }, status: :not_found }
+    end
   end
 
   def server_error
-    render 'server_error', status: :internal_server_error
+    respond_to do |format|
+      format.html { render 'server_error', status: :internal_server_error }
+      format.any { render json: { message: 'Server error' }, status: :internal_server_error }
+    end
   end
 end
