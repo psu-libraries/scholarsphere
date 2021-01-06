74b7262def0e79a9d6345f6c09f242c3e9c7db35
Adam Wead <awead@users.noreply.github.com>
2021-01-06 10:07:02 -0500

    Refactor Google Scholar meta tags
    Adds meta tags for links to the files in the work, enabling Google to
    perform fulltext indexing. Additionally, collections will no longer have
    Google Scholar meta tags.
    
    Refactoring removes previous code from the ResourceDecorator and puts
    it in a newly created GoogleScholarMetadataComponent. This new component
    encapsulates all the logic and content required to render the tags.

---
+++ app/components/google_scholar_metadata_component.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+class GoogleScholarMetadataComponent < ApplicationComponent
+  attr_reader :resource,
+              :policy
+
+  delegate :title,
+           :creator_aliases,
+           :deposited_at,
+           :published_date,
+           to: :resource
+
+  # @param [ResourceDecorator] resource
+  # @param [ApplicationPolicy] policy
+  def initialize(resource:, policy:)
+    @resource = resource
+    @policy = policy
+  end
+
+  def render?
+    resource.is_a?(WorkVersionDecorator)
+  end
+
+  def citation_authors
+    creator_aliases.map(&:alias)
+  end
+
+  def citation_publication_date
+    return deposited_at.year unless EdtfDate.valid?(published_date)
+
+    Date.edtf(published_date).year
+  end
+
+  def file_resources
+    return [] unless policy.download?
+
+    resource.file_resources
+  end
+end
