1f88f81ff3114bdaf63263dbda41bad938e399fd
Adam Wead <awead@users.noreply.github.com>
2021-01-13 11:48:41 -0500

    Add upload endpoint to API
    Returns a presigned url to a client so that they may upload a file into
    our S3 instance. The endpoint requires an extension, such as pdf, or
    txt, so that it can construct a path for the file in the bucket.
    
    Filenames are unique UUIDs, matching the current behavior in the UI via
    the Shrine/Uppy process.

---
+++ spec/controllers/api/v1/uploads_controller_spec.rb
@@ -0,0 +1,47 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Api::V1::UploadsController, type: :controller do
+  let(:api_token) { create(:api_token).token }
+
+  before { request.headers[:'X-API-Key'] = api_token }
+
+  describe 'POST #create' do
+    let(:presigned_url) { JSON.parse(response.body)['url'] }
+    let(:id) { JSON.parse(response.body)['id'] }
+    let(:prefix) { JSON.parse(response.body)['prefix'] }
+
+    before { post :create, params: { extension: extension } }
+
+    context 'with a valid extension' do
+      let(:extension) { Faker::File.extension }
+
+      it 'creates a presigned url' do
+        expect(response).to be_ok
+        expect(presigned_url).to include(ENV['S3_ENDPOINT'])
+        expect(URI.parse(presigned_url).path).to match(
+          "/#{ENV['AWS_BUCKET']}/#{Scholarsphere::ShrineConfig::CACHE_PREFIX}.*#{extension}"
+        )
+        expect(id).to end_with(extension)
+        expect(prefix).to eq(Scholarsphere::ShrineConfig::CACHE_PREFIX)
+      end
+    end
+
+    context 'when the extension includes the period' do
+      let(:extension) { ".#{Faker::File.extension}" }
+
+      it 'creates a presigned url without the duplicate period' do
+        expect(response).to be_ok
+        expect(URI.parse(presigned_url).path).not_to include('..')
+        expect(id).not_to include('..')
+      end
+    end
+
+    context 'with a missing key' do
+      let(:extension) { nil }
+
+      it { expect(response).to be_bad_request }
+    end
+  end
+end
