5acab47db49e5a338539595bea6ffc5ed2cea479
Adam Wead <awead@users.noreply.github.com>
2021-02-26 14:16:53 -0500

    Return to withdrawn work version from settings page
    When viewing a work's settings, if its only version is withdrawn, we
    should return to the version's resource page and not work's.

--- app/components/back_to_work_button_component.rb
+++ app/components/back_to_work_button_component.rb
@@ -7,12 +7,13 @@ class BackToWorkButtonComponent < ApplicationComponent
     @work = work
   end
 
-  # If the work has not been published, that is if it _only_ has a draft
-  # version, then we want to go to the resource page for that draft version.
-  # Otherwise, we want to to the resoure page of the work itself.
+  # If the work has not been published, that is if it _only_ has a draft version, then we want to go to the resource
+  # page for that draft version.  If the work is withdrawn, or for some reason there's no draft version, we'll go to the
+  # resource page for the first (likely withdrawn) version.  Otherwise, we want to to the resource page of the work
+  # itself.
   def path
     resource = if work.latest_published_version.nil?
-                 work.draft_version
+                 work.draft_version || work.versions[0]
                else
                  work
                end
