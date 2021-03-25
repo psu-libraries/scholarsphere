3007af64cd0add990c97ad3ebe5c3367beb63619
Ryan Schenk <rschenk@gmail.com>
2021-03-25 13:34:50 -0400

    Object for searching for DOIs in Solr

---
+++ app/models/doi_search.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+class DoiSearch
+  attr_reader :doi
+
+  def initialize(doi:)
+    @doi = Doi.new(doi)
+  end
+
+  # @returns an array of uuids for the matching resources
+  def results
+    return [] unless doi.valid?
+
+    doi_query = escape(doi.to_s)
+
+    solr_response = Blacklight.default_index.search(
+      q: %(all_dois_ssim:"#{doi_query}"),
+      fl: ['id'],
+      rows: max_documents
+    )
+
+    solr_response
+      .response
+      .fetch('docs', [])
+      .map { |d| d['id'] }
+  end
+
+  private
+
+    def max_documents
+      1_000
+    end
+
+    def escape(value)
+      RSolr.solr_escape(value)
+    end
+end
---
+++ spec/models/doi_search_spec.rb
@@ -0,0 +1,42 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe DoiSearch do
+  let!(:work_version_1) { create :work_version, doi: 'doi:10.26207/upw2-pkx3' }
+  let!(:work_version_2) { create :work_version, doi: 'doi:10.18113/dmnf-6dzs', identifier: ['doi:10.26207/upw2-pkx3'] }
+  let!(:collection) { create :collection, doi: 'doi:10.18113/qi03-b693' }
+
+  before do
+    Work.reindex_all(async: false)
+    Collection.reindex_all
+  end
+
+  describe '#results' do
+    it 'returns an array of resource uuids who reference the given doi' do
+      # Find a collection
+      expect(described_class.new(doi: collection.doi).results)
+        .to contain_exactly(collection.uuid)
+
+      # Find a work version
+      # Note that w_v_2 duplicates w_v_1's doi in its identifier attribute
+      expect(described_class.new(doi: work_version_1.doi).results)
+        .to contain_exactly(work_version_1.uuid, work_version_2.uuid)
+
+      expect(described_class.new(doi: work_version_2.doi).results)
+        .to contain_exactly(work_version_2.uuid)
+
+      # Find a collection, with non-canonical formatting
+      expect(described_class.new(doi: 'https://doi.org/10.18113/qi03-b693').results)
+        .to contain_exactly(collection.uuid)
+
+      # Find nothing
+      expect(described_class.new(doi: 'nothing to see here').results)
+        .to eq []
+
+      # Find l337 h4x
+      expect(described_class.new(doi: '"} hacking in progress').results)
+        .to eq []
+    end
+  end
+end
