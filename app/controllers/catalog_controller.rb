baa9d0fb60c649e00b1f4e1cb77927da570d4ead
Adam Wead <awead@users.noreply.github.com>
2021-04-08 08:59:29 -0400

    Correct field searching
    Searching by title was broken due to a mis-configured Solr, and a bug in
    the search form. This corrects both issues, removes subject searching,
    and replaces an Author search with a Creator search.
    
    This also updates the default qf parameter in the Solr config. Although,
    this is unlikely to have any impact since Blacklight sends qf values to
    Solr at query time. However, it seemed a good idea to go ahead and
    update these values with reasonable defaults: the _tesim field of our
    main metadata fields. This will ensure any queries without an explicit
    qf value will search our most common fields.

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -182,23 +182,11 @@ class CatalogController < ApplicationController
       }
     end
 
-    config.add_search_field('author') do |field|
+    config.add_search_field('creator') do |field|
       field.solr_parameters = {
-        'spellcheck.dictionary': 'author',
-        qf: '${author_qf}',
-        pf: '${author_pf}'
-      }
-    end
-
-    # Specifying a :qt only to show it's possible, and so our internal automated
-    # tests can test it. In this case it's the same as
-    # config[:default_solr_parameters][:qt], so isn't actually neccesary.
-    config.add_search_field('subject') do |field|
-      field.qt = 'search'
-      field.solr_parameters = {
-        'spellcheck.dictionary': 'subject',
-        qf: '${subject_qf}',
-        pf: '${subject_pf}'
+        'spellcheck.dictionary': 'creator',
+        qf: '${creator_qf}',
+        pf: '${creator_pf}'
       }
     end
 
