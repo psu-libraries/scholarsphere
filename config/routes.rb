f6b18e7ca492a965f2d357ff1577d2d0f007652d
Seth Erickson <sr.erickson@gmail.com>
2021-04-12 11:28:08 -0400

    terms of use page: added to page footer and about page; change column width for markdown pages

--- config/routes.rb
+++ config/routes.rb
@@ -26,6 +26,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   get 'about', to: 'markdown#show', page: 'about'
   get 'help', to: 'markdown#show', page: 'help'
+  get 'terms', to: 'markdown#show', page: 'terms'
   get 'policies-1.0', to: 'markdown#show', page: 'policies_1_0'
   get 'policies-2.0', to: 'markdown#show', page: 'policies_2_0'
   get 'policies', to: 'markdown#show', page: 'policies_2_0'
