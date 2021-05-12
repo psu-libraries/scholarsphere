0d12f15ef33ca9632cb9759b7a14bd1e216c8736
Nick Tiberi <nicktiberi@gmail.com>
2021-05-12 14:43:28 -0400

    Update the visibility badge to indicate that a work is embargoed
    The embargoed state will now also show a tooltip on hover,
    indicating the embargo date.

--- spec/components/visibility_badge_component_spec.rb
+++ spec/components/visibility_badge_component_spec.rb
@@ -5,6 +5,7 @@ require 'rails_helper'
 RSpec.describe VisibilityBadgeComponent, type: :component do
   let(:node) { render_inline(described_class.new(work: work)) }
   let(:badge) { node.css('div').first }
+  let(:embargo_date) { Time.zone.now + 6.days }
 
   context 'when a work is open access' do
     let(:work) { build(:work) }
@@ -13,6 +14,19 @@ RSpec.describe VisibilityBadgeComponent, type: :component do
       expect(badge.text).to include(I18n.t('visibility_badge_component.label.open'))
       expect(badge.attributes['data-before'].value).to eq('lock_open')
       expect(badge.classes).to contain_exactly('badge', 'badge--icon', 'badge--icon-orange')
+      expect(badge['title']).to be_nil
+    end
+
+    context 'when a work is embargoed' do
+      let(:work) { build(:work, embargoed_until: embargo_date) }
+
+      specify do
+        expect(badge.text).to include(I18n.t('visibility_badge_component.label.embargoed'))
+        expect(badge.attributes['data-before'].value).to eq('lock_clock')
+        expect(badge.classes).to contain_exactly('badge', 'badge--icon', 'badge--icon-red')
+        expect(badge['title']).to eq(I18n.t('visibility_badge_component.tooltip.embargoed',
+                                            date: embargo_date.strftime('%Y-%m-%d')))
+      end
     end
   end
 
@@ -23,6 +37,19 @@ RSpec.describe VisibilityBadgeComponent, type: :component do
       expect(badge.text).to include(I18n.t('visibility_badge_component.label.authenticated'))
       expect(badge.attributes['data-before'].value).to eq('pets')
       expect(badge.classes).to contain_exactly('badge', 'badge--icon', 'badge--icon-blue')
+      expect(badge['title']).to be_nil
+    end
+
+    context 'when a work is embargoed' do
+      let(:work) { build(:work, embargoed_until: embargo_date, visibility: Permissions::Visibility::AUTHORIZED) }
+
+      specify do
+        expect(badge.text).to include(I18n.t('visibility_badge_component.label.embargoed'))
+        expect(badge.attributes['data-before'].value).to eq('lock_clock')
+        expect(badge.classes).to contain_exactly('badge', 'badge--icon', 'badge--icon-red')
+        expect(badge['title']).to eq(I18n.t('visibility_badge_component.tooltip.embargoed',
+                                            date: embargo_date.strftime('%Y-%m-%d')))
+      end
     end
   end
 
@@ -33,6 +60,19 @@ RSpec.describe VisibilityBadgeComponent, type: :component do
       expect(badge.text).to include(I18n.t('visibility_badge_component.label.restricted'))
       expect(badge.attributes['data-before'].value).to eq('lock')
       expect(badge.classes).to contain_exactly('badge', 'badge--icon', 'badge--icon-red')
+      expect(badge['title']).to be_nil
+    end
+
+    context 'when a work is embargoed' do
+      let(:work) { build(:work, embargoed_until: embargo_date, visibility: Permissions::Visibility::PRIVATE) }
+
+      specify do
+        expect(badge.text).to include(I18n.t('visibility_badge_component.label.embargoed'))
+        expect(badge.attributes['data-before'].value).to eq('lock_clock')
+        expect(badge.classes).to contain_exactly('badge', 'badge--icon', 'badge--icon-red')
+        expect(badge['title']).to eq(I18n.t('visibility_badge_component.tooltip.embargoed',
+                                            date: embargo_date.strftime('%Y-%m-%d')))
+      end
     end
   end
 end
