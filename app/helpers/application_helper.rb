273d41a6398b1e48c816da7082bb07062d51f0c8
Dann <whereismyjetpack@users.noreply.github.com>
2022-01-19 12:54:11 -0500

    Release v4.9.5 (#1175)
    * Bump graphiql from 1.4.2 to 1.4.7
    
    Bumps [graphiql](https://github.com/graphql/graphiql) from 1.4.2 to 1.4.7.
    - [Release notes](https://github.com/graphql/graphiql/releases)
    - [Changelog](https://github.com/graphql/graphiql/blob/main/CHANGELOG.md)
    - [Commits](https://github.com/graphql/graphiql/compare/graphiql@1.4.2...graphiql@1.4.7)
    
    ---
    updated-dependencies:
    - dependency-name: graphiql
      dependency-type: direct:production
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * Bump library/ruby-2.7.3-node-15 from 20211010 to 20211108
    
    Bumps library/ruby-2.7.3-node-15 from 20211010 to 20211108.
    
    ---
    updated-dependencies:
    - dependency-name: library/ruby-2.7.3-node-15
      dependency-type: direct:production
      update-type: version-update:semver-major
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * fix ci
    
    * Bump library/ruby-2.7.3-node-15 from 20211108 to 20211129
    
    Bumps library/ruby-2.7.3-node-15 from 20211108 to 20211129.
    
    ---
    updated-dependencies:
    - dependency-name: library/ruby-2.7.3-node-15
      dependency-type: direct:production
      update-type: version-update:semver-major
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * use DOCKER_PASSWORD
    
    * updates solr in docker to match what's in qa (#1165)
    
    * Bump library/ruby-2.7.3-node-15 from 20211129 to 20220110 (#1163)
    
    Bumps library/ruby-2.7.3-node-15 from 20211129 to 20220110.
    
    ---
    updated-dependencies:
    - dependency-name: library/ruby-2.7.3-node-15
      dependency-type: direct:production
      update-type: version-update:semver-major
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * updates redis (#1162)
    
    * removes unused docker-compose services. refactors startup for performance (#1169)
    
    * removes unused docker-compose services. refactors startup for performance
    
    * removes unused services
    
    * assumes using docker-for-mac. if not, allow override in envrc
    
    * Bump follow-redirects from 1.14.4 to 1.14.7 (#1168)
    
    Bumps [follow-redirects](https://github.com/follow-redirects/follow-redirects) from 1.14.4 to 1.14.7.
    - [Release notes](https://github.com/follow-redirects/follow-redirects/releases)
    - [Commits](https://github.com/follow-redirects/follow-redirects/compare/v1.14.4...v1.14.7)
    
    ---
    updated-dependencies:
    - dependency-name: follow-redirects
      dependency-type: indirect
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * Bump markdown-it from 12.2.0 to 12.3.2 (#1167)
    
    Bumps [markdown-it](https://github.com/markdown-it/markdown-it) from 12.2.0 to 12.3.2.
    - [Release notes](https://github.com/markdown-it/markdown-it/releases)
    - [Changelog](https://github.com/markdown-it/markdown-it/blob/master/CHANGELOG.md)
    - [Commits](https://github.com/markdown-it/markdown-it/compare/12.2.0...12.3.2)
    
    ---
    updated-dependencies:
    - dependency-name: markdown-it
      dependency-type: indirect
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * Markdown support for field hints' translations (#1172)
    
    * Update ci (#1173)
    
    * updates CI
    
    * re-add redis check
    
    * updates rails (#1171)
    
    * fix ci (#1174)
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    Co-authored-by: Adam Wead <awead@users.noreply.github.com>
    Co-authored-by: Ryan Schenk <ryan@westarete.com>

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -27,13 +27,4 @@ module ApplicationHelper
       link_to link, path, options.merge(class: 'nav-link')
     end
   end
-
-  # @note Based off of ActionView::Helpers::Tags::Base#tag_id. Because it's a private method, and we shouldn't override
-  # it, we're redoing it here.
-  def form_field_id(form, attribute)
-    sanitized_object_name = form.object_name
-      .gsub(/\]\[|[^-a-zA-Z0-9:.]/, '_') # Replace non-alphanumerics with '_'
-      .sub(/_$/, '')                     # Remove a trailing '_'
-    [sanitized_object_name, attribute.to_s].join('_')
-  end
 end
