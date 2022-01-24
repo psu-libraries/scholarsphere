010e5d652c1f5611c4eaf64cae75ac26307fd1a2
Alex Kiessling <32677188+ajkiessl@users.noreply.github.com>
2022-01-24 12:51:51 -0500

    Limiting the dois returned from AllDois for WorkVersions (#1180)

--- spec/models/concerns/all_dois_spec.rb
+++ spec/models/concerns/all_dois_spec.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require 'spec_helper'
+require 'rails_helper'
 
 RSpec.describe AllDois do
   subject(:instance) { TestModel.new }
@@ -41,10 +41,50 @@ RSpec.describe AllDois do
   end
 
   describe '#all_dois' do
-    it 'filters all values returned by #fields_with_dois and returns canonical DOIs' do
-      expect(instance.all_dois).to match_array(['doi:10.18113/3ln3-2by1',
-                                                'doi:10.1515/pol-2020-2011',
-                                                'doi:10.1007/s10570-013-0029-x'])
+    context 'when resource is not a WorkVersion' do
+      it 'filters all values returned by #fields_with_dois and returns canonical DOIs' do
+        expect(instance.all_dois).to match_array(['doi:10.18113/3ln3-2by1',
+                                                  'doi:10.1515/pol-2020-2011',
+                                                  'doi:10.1007/s10570-013-0029-x'])
+      end
+    end
+
+    context 'when resource is a WorkVersion' do
+      subject(:instance) { work.versions.last }
+
+      let!(:work) { create :work, versions_count: 2, has_draft: false }
+
+      before do
+        instance.update doi: '10.18113/s9k3-x5gh'
+      end
+
+      context 'when WorkVersion is not the lastest published version' do
+        before do
+          instance.update aasm_state: 'draft'
+        end
+
+        it 'returns an empty array' do
+          expect(instance.all_dois).to eq []
+        end
+      end
+
+      context 'when WorkVersion is the lastest published version' do
+        context "when WorkVersion's Work has a DOI" do
+          before do
+            instance.work.doi = '10.18113/44md-dj4'
+          end
+
+          it 'returns an empty array' do
+            expect(instance.all_dois).to eq []
+          end
+        end
+
+        context "when WorkVersion's Work does not have a DOI" do
+          it 'filters all values returned by #fields_with_dois and returns canonical DOIs' do
+            expect(instance.all_dois).to match_array(['doi:10.18113/s9k3-x5gh'])
+          end
+        end
+      end
     end
   end
 end
--- spec/models/doi_search_spec.rb
+++ spec/models/doi_search_spec.rb
@@ -3,9 +3,12 @@
 require 'rails_helper'
 
 RSpec.describe DoiSearch do
-  let!(:work_version_1) { create :work_version, doi: '10.26207/upw2-pkx3' }
-  let!(:work_version_2) { create :work_version, doi: '10.18113/dmnf-6dzs', identifier: ['doi:10.26207/upw2-pkx3'] }
-  let!(:collection) { create :collection, doi: '10.18113/qi03-b693' }
+  let!(:work_version_1) { create :work_version, :published, doi: '10.26207/rb4s-33xs' }
+  let!(:work_version_2) { create :work_version, :published, doi: '10.26207/jjd7-0is4', work: work_1 }
+  let!(:work_1) { create :work, doi: '10.18113/dmnf-6dzs' }
+  let(:work_2) { create :work, versions_count: 4, has_draft: false, doi: nil }
+  let!(:collection_1) { create :collection, doi: '10.18113/dmnf-6dzs', identifier: ['doi:10.26207/upw2-pkx3'] }
+  let!(:collection_2) { create :collection, doi: '10.18113/qi03-b693' }
 
   before do
     Work.reindex_all(async: false)
@@ -16,9 +19,10 @@ RSpec.describe DoiSearch do
     it 'returns all unique DOIs in the index' do
       expect(described_class.all).to match(
         {
-          'doi:10.26207/upw2-pkx3' => a_collection_containing_exactly(work_version_1.uuid, work_version_2.uuid),
-          'doi:10.18113/dmnf-6dzs' => [work_version_2.uuid],
-          'doi:10.18113/qi03-b693' => [collection.uuid]
+          'doi:10.26207/rb4s-33xs' => [work_version_1.uuid],
+          'doi:10.26207/upw2-pkx3' => [collection_1.uuid],
+          'doi:10.18113/dmnf-6dzs' => a_collection_containing_exactly(work_1.uuid, collection_1.uuid),
+          'doi:10.18113/qi03-b693' => [collection_2.uuid]
         }
       )
     end
@@ -27,20 +31,24 @@ RSpec.describe DoiSearch do
   describe '#results' do
     it 'returns an array of resource uuids who reference the given doi' do
       # Find a collection
-      expect(described_class.new(doi: collection.doi).results)
-        .to contain_exactly(collection.uuid)
+      expect(described_class.new(doi: collection_2.doi).results)
+        .to contain_exactly(collection_2.uuid)
 
-      # Find a work version
-      # Note that w_v_2 duplicates w_v_1's doi in its identifier attribute
+      # Find work version 1 because it is the latest published version and its work has no doi
       expect(described_class.new(doi: work_version_1.doi).results)
-        .to contain_exactly(work_version_1.uuid, work_version_2.uuid)
+        .to contain_exactly(work_version_1.uuid)
 
+      # Cannot find work version 2 because work 1 has a doi
       expect(described_class.new(doi: work_version_2.doi).results)
-        .to contain_exactly(work_version_2.uuid)
+        .to eq []
 
       # Find a collection, with non-canonical formatting
       expect(described_class.new(doi: 'https://doi.org/10.18113/qi03-b693').results)
-        .to contain_exactly(collection.uuid)
+        .to contain_exactly(collection_2.uuid)
+
+      # Find the work and collection combo
+      expect(described_class.new(doi: collection_1.doi).results)
+        .to contain_exactly(work_1.uuid, collection_1.uuid)
 
       # Find nothing
       expect(described_class.new(doi: 'nothing to see here').results)
