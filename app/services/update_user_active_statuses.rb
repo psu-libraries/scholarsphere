4d9d2e9db103484371d44f6425eed398887bd8fb
Alex Kiessling <ajkiessl@gmail.com>
2022-01-26 11:14:31 -0500

    Refactored to quiet codeclimate

--- app/services/update_user_active_statuses.rb
+++ app/services/update_user_active_statuses.rb
@@ -1,17 +1,25 @@
 # frozen_string_literal: true
 
 class UpdateUserActiveStatuses
-  def self.call
-    User.find_each do |user|
-      begin
-        identity = PsuIdentity::SearchService::Client.new.userid(user.access_id)
-        user.active = (identity.affiliation != ['MEMBER'])
-      rescue PsuIdentity::SearchService::NotFound
-        user.active = false
+  class << self
+    def call
+      User.find_each do |user|
+        begin
+          user.active = active?(user)
+        rescue PsuIdentity::SearchService::NotFound
+          user.active = false
+        end
+        user.save!
+      rescue PsuIdentity::SearchService::Error, Net::ReadTimeout, Net::OpenTimeout, SocketError
+        next
       end
-      user.save!
-    rescue PsuIdentity::SearchService::Error, Net::ReadTimeout, Net::OpenTimeout, SocketError
-      next
     end
+
+    private
+
+      def active?(user)
+        identity = PsuIdentity::SearchService::Client.new.userid(user.access_id)
+        identity.affiliation != ['MEMBER']
+      end
   end
 end
