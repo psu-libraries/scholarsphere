d1c2851247ecc77e8bcad9d1e04154b5cfb048ae
Nick Tiberi <nicktiberi@gmail.com>
2022-02-02 09:10:02 -0500

    New service to merge a collection and its works into a single work
    This service will be used via rake tasks to merge collections with
    many single-file works to be merged into a single work with many files.
    
    Co-authored-by: Ryan Schenk <rschenk@gmail.com>

---
+++ app/services/merge_collection.rb
@@ -0,0 +1,221 @@
+# frozen_string_literal: true
+
+class MergeCollection
+  class << self
+    def call(uuid, delete_collection: true)
+      instance = new(uuid, delete_collection)
+      instance.save
+      instance
+    end
+  end
+
+  attr_reader :uuid,
+              :delete_collection,
+              :errors
+
+  def initialize(uuid, delete_collection)
+    @uuid = uuid
+    @delete_collection = delete_collection
+    @errors = []
+    @successful = false # updated by #save
+  end
+
+  def save
+    return false if collection.nil?
+    return false unless valid?
+
+    ActiveRecord::Base.transaction do
+      PaperTrail.request(whodunnit: paper_trail_user) do
+        new_work = merge_collection
+
+        if !new_work.valid?
+          new_work.errors.full_messages.each { |err| errors << err }
+          return false
+        end
+
+        new_work.save!
+
+        new_work.update_index
+      end
+
+      if delete_collection?
+        collection.destroy!
+
+        collection.works.each do |work|
+          work.versions.each do |version|
+            DestroyWorkVersion.call(version, force: true)
+          end
+        end
+      end
+    end
+
+    @successful = true
+    true
+  end
+
+  def delete_collection?
+    @delete_collection
+  end
+
+  def valid?
+    validate
+    errors.empty?
+  end
+
+  def successful?
+    @successful
+  end
+
+  def validate
+    collection_works = collection.works
+    canonical_metadata = work_metadata(canonical_work)
+
+    collection_works.each do |w|
+      w_metadata = work_metadata(w)
+      work_metadata_diff = MetadataDiff.call(
+        OpenStruct.new(metadata: canonical_metadata),
+        OpenStruct.new(metadata: w_metadata)
+      )
+
+      if w_metadata[:num_work_versions] != 1
+        errors << "Work-#{w.id} has #{w_metadata[:num_work_versions]} work versions, but must only have 1"
+      end
+
+      if w_metadata[:num_files] != 1
+        errors << "Work-#{w.id} has #{w_metadata[:num_files]} files, but must only have 1"
+      end
+
+      if work_metadata_diff.any?
+        diff = work_metadata_diff.inspect
+        errors << "Work-#{canonical_work.id} has different work metadata than Work-#{w.id}\n#{diff}"
+      end
+
+      if !w.representative_version.published?
+        errors << "Work-#{canonical_work.id} is not published"
+      end
+
+      validate_access_controls(w)
+      validate_work_version_metadata(w)
+      validate_creators(w)
+    end
+  end
+
+  private
+
+    def collection
+      @collection ||= Collection.find_by(uuid: uuid)
+    end
+
+    def canonical_work
+      @canonical_work ||= collection.works.first
+    end
+
+    def canonical_work_version
+      @canonical_work_version ||= canonical_work.versions.first
+    end
+
+    def paper_trail_user
+      @paper_trail_user ||= canonical_work_version.versions.first&.whodunnit
+    end
+
+    def validate_access_controls(work)
+      %i[discover read edit].each do |level|
+        %i[users groups].each do |agent|
+          if canonical_work.send("#{level}_#{agent}").map(&:id) != work.send("#{level}_#{agent}").map(&:id)
+            w_access = work.send("#{level}_#{agent}").inspect
+            canonical_access = canonical_work.send("#{level}_#{agent}").inspect
+            diff = "#{canonical_access}\n#{w_access}"
+
+            errors << "Work-#{canonical_work.id} has different #{level} #{agent} than Work-#{work.id}\n#{diff}"
+          end
+        end
+      end
+    end
+
+    def validate_creators(work)
+      if canonical_work_version.creators.map(&:actor_id) != work.representative_version.creators.map(&:actor_id)
+        w_creators = work.representative_version.creators.inspect
+        canonical_creators = canonical_work_version.creators.inspect
+        diff = "#{canonical_creators}\n#{w_creators}"
+
+        errors << "Work-#{canonical_work.id} has different creators than Work-#{work.id}\n#{diff}"
+      end
+    end
+
+    def validate_work_version_metadata(work)
+      work_version_metadata_diff = MetadataDiff.call(
+        canonical_work_version,
+        work.representative_version
+      )
+
+      attributes_we_dont_care_about = %i[title]
+      work_version_metadata_diff = work_version_metadata_diff.reject do |k, _v|
+        attributes_we_dont_care_about.include?(k.to_sym)
+      end
+
+      if work_version_metadata_diff.any?
+        printable_diff = work_version_metadata_diff.inspect
+        errors << "Work-#{canonical_work.id} has different WorkVersion metadata than Work-#{work.id}\n#{printable_diff}"
+      end
+    end
+
+    def merge_collection
+      deposited_at = collection
+        .works
+        .map(&:deposited_at)
+        .compact
+        .min
+
+      all_file_resources = collection
+        .works
+        .map(&:representative_version)
+        .map(&:file_resources)
+        .flatten
+
+      work = Work.build_with_empty_version(
+        work_type: canonical_work.work_type,
+        embargoed_until: canonical_work.embargoed_until,
+        doi: canonical_work.doi,
+        depositor: canonical_work.depositor,
+        proxy_depositor: canonical_work.proxy_depositor,
+        deposited_at: deposited_at
+      )
+
+      work.access_controls = canonical_work.access_controls.map(&:dup)
+
+      version = work.versions.first
+      version.metadata = canonical_work_version.metadata
+      version.creators = canonical_work_version.creators.map(&:dup)
+      version.file_resources = all_file_resources
+      version.view_statistics = aggregate_view_statistics
+      version.title = collection.title
+      version.publish
+
+      work
+    end
+
+    def work_metadata(work)
+      {
+        work_type: work.work_type,
+        embargoed_until: work.embargoed_until,
+        doi: work.doi,
+        depositor_id: work.depositor&.id,
+        proxy_depositor_id: work.proxy_depositor&.id,
+        num_work_versions: work.versions.count,
+        num_files: work.representative_version&.file_resources&.count
+      }
+    end
+
+    def aggregate_view_statistics
+      models_to_aggregate = [
+        collection,
+        collection.works.map(&:representative_version)
+      ].flatten
+
+      aggregated_stats = AggregateViewStatistics.call(models: models_to_aggregate)
+
+      aggregated_stats.map do |date, count, _running_total|
+        ViewStatistic.new(date: date, count: count)
+      end
+    end
+end
---
+++ spec/services/merge_collection_spec.rb
@@ -0,0 +1,195 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe MergeCollection do
+  subject(:merge_result) { described_class.call(collection.uuid) }
+
+  let(:collection) { create(:collection, works: [work1, work2]) }
+  let(:user) { create(:user) }
+  let(:actor) { user.actor }
+
+  let(:work1) { create(:work, has_draft: false, depositor: actor) }
+  let(:work2) { create(:work, has_draft: false, depositor: actor) }
+  let(:common_metadata) { attributes_for(:work_version, :published, :with_complete_metadata) }
+
+  # Update both works to have identical metadata on their versions, so we can induce various error states easily
+  before do
+    work1.versions.first.update(common_metadata)
+    work2.versions.first.update(common_metadata)
+
+    work2.versions.first.creators = work1.versions.first.creators.map(&:dup)
+    work2.save!
+  end
+
+  context 'when the works in the collection are inelegible to be merged' do
+    context 'when a work has too many versions' do
+      let(:work1) { create(:work, versions_count: 1, has_draft: false) }
+      let(:work2) { create(:work, versions_count: 2, has_draft: false) }
+
+      it 'returns an error message' do
+        error_msg = "Work-#{work2.id} has 2 work versions, but must only have 1"
+        expect(merge_result.errors).to include(a_string_matching(error_msg))
+      end
+    end
+
+    context 'when a work has too many files' do
+      let (:work1) { create(:work, versions: [v1], depositor: actor) }
+      let (:work2) { create(:work, has_draft: false, depositor: actor) }
+      let (:v1) { create(:work_version, :with_files, :published, file_count: 2) }
+
+      it 'returns an error message' do
+        expect(merge_result.errors).to include("Work-#{work1.id} has 2 files, but must only have 1")
+      end
+    end
+
+    context 'when the works in the collection have mismatched work-level metadata' do
+      let(:work1) { create(:work, has_draft: false) }
+      let(:work2) { create(:work, has_draft: false) }
+
+      it 'returns an error message' do
+        error_msg = /Work-#{work1.id} has different work metadata than Work-#{work2.id}/i
+        expect(merge_result.errors).to include(a_string_matching(error_msg))
+      end
+    end
+
+    context 'when the resulting work has an ActiveRecord validation error' do
+      before do
+        collection # create collection in db
+
+        # Force work1 to be invalid
+        work1.work_type = nil
+        work1.save(validate: false)
+        work2.work_type = nil
+        work2.save(validate: false)
+      end
+
+      it 'adds any ActiveRecord valication errors on the new work to the errors array' do
+        expect(merge_result.errors).to include(a_string_matching(/work type can't be blank/i))
+      end
+    end
+
+    context 'when the works in the collection have mismatched version-level metadata' do
+      before do
+        work1.versions.first.update(description: 'new description')
+      end
+
+      it 'returns an error message' do
+        error_msg = /Work-#{work1.id} has different WorkVersion metadata than Work-#{work2.id}/i
+        expect(merge_result.errors).to include(a_string_matching(error_msg))
+        expect(merge_result.errors).to include(a_string_matching(/new description/i))
+      end
+    end
+
+    context 'when a work in the collection is not published' do
+      before do
+        work1.versions.first.update(aasm_state: 'draft')
+      end
+
+      it 'returns an error message' do
+        expect(merge_result.errors).to include("Work-#{work1.id} is not published")
+      end
+    end
+
+    context 'when the works in the collection have mismatched access controls' do
+      let(:edit_user) { create(:user) }
+      let(:work1) { create(:work, has_draft: false, depositor: actor, edit_users: [edit_user]) }
+      let(:work2) { create(:work, has_draft: false, depositor: actor) }
+
+      it 'returns an error message' do
+        error_msg = /Work-#{work1.id} has different discover users than Work-#{work2.id}/i
+        expect(merge_result.errors).to include(a_string_matching(error_msg))
+      end
+    end
+
+    context 'when the works in the collection have mismatched creators' do
+      before do
+        work2.versions.first.creators = build_list(:authorship, 1)
+        work2.save!
+      end
+
+      it 'returns an error message' do
+        error_msg = /Work-#{work1.id} has different creators than Work-#{work2.id}/i
+        expect(merge_result.errors).to include(a_string_matching(error_msg))
+      end
+    end
+  end
+
+  context 'when the database transaction fails' do
+    before do
+      allow(DestroyWorkVersion).to receive(:call).and_raise(StandardError)
+    end
+
+    it 'rolls back all changes' do
+      expect {
+        begin
+          merge_result
+        rescue StandardError
+          # noop
+        end
+      }.not_to change(Work, :count)
+
+      expect(collection.reload).to be_present
+    end
+  end
+
+  context 'when the works in the collection are eligible to be merged' do
+    before do
+      create(:view_statistic, resource: collection, date: Date.parse('2022-02-06'), count: 1)
+      create(:view_statistic, resource: collection, date: Date.parse('2022-02-07'), count: 1)
+      create(:view_statistic, resource: work1.versions.first, date: Date.parse('2022-02-07'), count: 1)
+    end
+
+    it 'merges the works into a single work' do
+      expect(merge_result).to be_successful
+
+      new_work = Work.last
+      expect(new_work.versions.length).to eq 1
+
+      version = new_work.versions.first
+
+      # spot check attributes
+      expect(version).to be_published
+      expect(version.title).to eq collection.title
+      expect(version.rights).to eq common_metadata[:rights]
+      expect(version.description).to eq common_metadata[:description]
+      expect(version.published_date).to eq common_metadata[:published_date]
+
+      # check files
+      original_files = [
+        work1.versions.first.file_resources,
+        work2.versions.first.file_resources
+      ].flatten
+      expect(version.file_resources).to match_array(original_files)
+
+      # check creators
+      expect(version.creators.map(&:display_name)).to match_array(work1.versions.first.creators.map(&:display_name))
+
+      # check view stats
+      expect(
+        version.view_statistics.map { |vs| [vs.date, vs.count] }
+      ).to match_array([
+                         [Date.parse('2022-02-06'), 1],
+                         [Date.parse('2022-02-07'), 2]
+                       ])
+    end
+
+    context 'when the delete_collection param is true' do
+      it 'deletes the original collection and its works' do
+        merge_result
+        expect { collection.reload }.to raise_error(ActiveRecord::RecordNotFound)
+        expect { work1.reload }.to raise_error(ActiveRecord::RecordNotFound)
+        expect { work2.reload }.to raise_error(ActiveRecord::RecordNotFound)
+      end
+    end
+
+    context 'when the delete_collection param is false' do
+      it 'does not delete the original collection or its works' do
+        described_class.call(collection.uuid, delete_collection: false)
+        expect(collection.reload).to be_present
+        expect(work1.reload).to be_present
+        expect(work2.reload).to be_present
+      end
+    end
+  end
+end
