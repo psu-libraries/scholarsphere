c62cefa9fe473f50b81b554f854c4d739fce87e8
whereismyjetpack <djb44@psu.edu>
2022-02-02 20:23:35 -0500

    try this

--- spec/jobs/shrine/promotion_job_spec.rb
+++ spec/jobs/shrine/promotion_job_spec.rb
@@ -11,6 +11,7 @@ RSpec.describe Shrine::PromotionJob, type: :job do
     let(:mock_attacher) { instance_spy(FileUploader::Attacher) }
 
     before { allow(Shrine::Attacher).to receive(:retrieve).and_return(mock_attacher) }
+    before { allow(Shrine::ThumbnailJob).to receive(:perform).and return(mock_attacher) }
 
     it 'promotes a file from cache to storage' do
       described_class.perform_now(record: record, name: name, file_data: file_data)
