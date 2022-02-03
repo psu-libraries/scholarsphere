3201f359faf9f430626f16cc0dba6754be742f0e
Alex Kiessling <ajkiessl@gmail.com>
2022-02-03 09:56:13 -0500

    Basic display logic for thumbnails.  Should be its own service class or something.

--- app/components/thumbnail_component.rb
+++ app/components/thumbnail_component.rb
@@ -9,6 +9,10 @@ class ThumbnailComponent < ApplicationComponent
     @featured = featured
   end
 
+  def thumbnail_url
+    file_resources.present? ? file_resources.last.file_derivatives[:thumbnail]&.url : nil
+  end
+
   def icon
     icon_map.fetch(icon_key, 'bar_chart')
   end
@@ -22,6 +26,18 @@ class ThumbnailComponent < ApplicationComponent
   end
 
   private
+
+    def file_resources
+      work.latest_published_version.file_resources
+    end
+
+    def work
+      resource.is_a?(Collection) ? resource.works.first.work : solr_doc_to_work(resource)
+    end
+
+    def solr_doc_to_work(solr_doc)
+      solr_doc["model_ssi"] == 'Collection' ? Collection.find_by(uuid: solr_doc.id).works.first : Work.find(resource.work_id)
+    end
 
     def featured?
       @featured != false
