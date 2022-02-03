1c4b75b54aaf4ac2598e7dadb11d2e0d1074c9ad
whereismyjetpack <djb44@psu.edu>
2022-02-02 20:10:05 -0500

    rubocop

--- app/jobs/shrine/thumbnail_job.rb
+++ app/jobs/shrine/thumbnail_job.rb
@@ -1,14 +1,13 @@
 # frozen_string_literal: true
 
 class Shrine::ThumbnailJob < ApplicationJob
-    queue_as :shrine
-  
-    def perform(record)
-        attacher = record.file_attacher
-        attacher.create_derivatives
-        record.save
-    rescue Shrine::AttachmentChanged, ActiveRecord::RecordNotFound
-      # attachment has changed or record has been deleted, nothing to do
-    end
+  queue_as :shrine
+
+  def perform(record)
+    attacher = record.file_attacher
+    attacher.create_derivatives
+    record.save
+  rescue Shrine::AttachmentChanged, ActiveRecord::RecordNotFound
+    # attachment has changed or record has been deleted, nothing to do
   end
-  
+end
