10e2839f9bfd4788ea6acd53873a955cba7384ca
whereismyjetpack <djb44@psu.edu>
2022-02-04 13:05:53 -0500

    rubocop

--- app/jobs/shrine/promotion_job.rb
+++ app/jobs/shrine/promotion_job.rb
@@ -6,7 +6,6 @@ class Shrine::PromotionJob < ApplicationJob
   def perform(record:, name:, file_data:)
     attacher = Shrine::Attacher.retrieve(model: record, name: name.to_sym, file: file_data)
     attacher.atomic_promote
-    binding.pry
     Shrine::ThumbnailJob.perform_later(record)
   rescue Shrine::AttachmentChanged, ActiveRecord::RecordNotFound
     # attachment has changed or record has been deleted, nothing to do
