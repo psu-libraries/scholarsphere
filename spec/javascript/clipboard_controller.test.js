53fa6ac2042824f4c5b937f4e8943aa0581f1ad5
Nick Tiberi <nicktiberi@gmail.com>
2022-02-16 10:08:47 -0500

    add test for clipboard_controller.js

---
+++ spec/javascript/clipboard_controller.test.js
@@ -0,0 +1,27 @@
+import { Application } from 'stimulus'
+import ClipboardController from 'clipboard_controller'
+
+Object.assign(navigator, {
+  clipboard: {
+    writeText: () => jest.fn().mockImplementation(() => Promise.resolve())
+  }
+})
+
+describe('ClipboardController', () => {
+  jest.spyOn(navigator.clipboard, 'writeText')
+
+  beforeEach(() => {
+    document.body.innerHTML =
+      '<span data-controller="clipboard" data-target="clipboard.source" data-signature="test">' +
+      '  <button id="btn" data-action="clipboard#copy">' +
+      '</span>'
+
+    const application = Application.start()
+    application.register('clipboard', ClipboardController)
+  })
+
+  it('copies the value to the clipboard when the button is clicked', () => {
+    document.getElementById('btn').click()
+    expect(navigator.clipboard.writeText).toHaveBeenCalledWith('test')
+  })
+})
