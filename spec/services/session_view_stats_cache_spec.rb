bb97afda98e6970abf4cc0afee70b069240c1b00
Ryan Schenk <rschenk@gmail.com>
2022-02-22 11:43:32 -0500

    Fix bug where stats cache was undercounting

---
+++ spec/services/session_view_stats_cache_spec.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe SessionViewStatsCache do
+  let(:session) { double('session', id: 1) }
+  let(:another_session) { double('session', id: 2) }
+  let(:work) { build_stubbed :work, id: 1 }
+  let(:collection) { build_stubbed :collection, id: 1 }
+
+  it 'retuns false if the given session/resource combo is in the cache already' do
+    expect(described_class.call(session: session, resource: work)).to eq true
+    expect(described_class.call(session: session, resource: work)).to eq false
+
+    expect(described_class.call(session: session, resource: collection)).to eq true
+    expect(described_class.call(session: session, resource: collection)).to eq false
+
+    expect(described_class.call(session: another_session, resource: work)).to eq true
+    expect(described_class.call(session: another_session, resource: collection)).to eq true
+  end
+end
