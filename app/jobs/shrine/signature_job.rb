a8356a601ddcfbdd66629bf93a6cc44013019204
whereismyjetpack <djb44@psu.edu>
2022-03-04 16:28:19 -0500

    run jobs with locking

--- app/jobs/shrine/signature_job.rb
+++ app/jobs/shrine/signature_job.rb
@@ -5,13 +5,15 @@ class Shrine::SignatureJob < ApplicationJob
   queue_as :signature
 
   def perform(file_resource:)
-    attacher = file_resource.file_attacher
-    io = Shrine.uploaded_file(attacher.file_data)
-    attacher.file.add_metadata(
-      'sha256' => Shrine.signature(io, :sha256),
-      'md5' => Shrine.signature(io, :md5)
-    )
-    attacher.write
-    file_resource.save
+    file_resource.with_lock do
+      attacher = file_resource.file_attacher
+      io = Shrine.uploaded_file(attacher.file_data)
+      attacher.file.add_metadata(
+        'sha256' => Shrine.signature(io, :sha256),
+        'md5' => Shrine.signature(io, :md5)
+      )
+      attacher.write
+      file_resource.save
+    end
   end
 end
