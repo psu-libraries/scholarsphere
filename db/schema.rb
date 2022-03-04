a71a61d99f464b328d093fe0778f3aeb3d2b6c11
Alex Kiessling <ajkiessl@gmail.com>
2022-03-04 14:37:57 -0500

    Adds pg extension.  Some wording changes in tests

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,7 @@
 ActiveRecord::Schema.define(version: 2022_02_24_191232) do
 
   # These are extensions that must be enabled in order to support this database
+  enable_extension "pg_stat_statements"
   enable_extension "plpgsql"
   enable_extension "uuid-ossp"
 
--- spec/support/shared/a_resource_with_thumbnail_settings.rb
+++ spec/support/shared/a_resource_with_thumbnail_settings.rb
@@ -1,13 +1,13 @@
 # frozen_string_literal: true
 
 RSpec.shared_examples 'a resource with thumbnail settings' do
-  describe 'selecting Use Automatically Generated Image' do
+  describe 'selecting the "Use Automatically Generated Image" radio buttonn' do
     context 'when no auto generated thumbnail exists for the resource' do
       before do
         visit Rails.application.routes.url_helpers.send("edit_dashboard_#{resource.class.to_s.downcase}_path", resource)
       end
 
-      it 'disables the Use Automatically Generated Image radio button is disabled', js: true do
+      it '"Use Automatically Generated Image" radio button is disabled', js: true do
         expect(page).to have_content('No Image').twice
         expect(page).to have_selector('input[id=thumbnail_form_thumbnail_selection_auto_generated][disabled=disabled]')
       end
@@ -20,7 +20,8 @@ RSpec.shared_examples 'a resource with thumbnail settings' do
         visit Rails.application.routes.url_helpers.send("edit_dashboard_#{resource.class.to_s.downcase}_path", resource)
       end
 
-      it 'works from the Settings page' do
+      it 'shows auto-generated image and the radio button is enabled' do
+        expect(page).to have_selector('img[src="url.com/path/file"]')
         expect(page).to have_content('No Image').once
         expect(page)
           .not_to have_selector('input[id=thumbnail_form_thumbnail_selection_auto_generated][disabled=disabled]')
@@ -28,17 +29,16 @@ RSpec.shared_examples 'a resource with thumbnail settings' do
         click_button I18n.t!('dashboard.shared.thumbnail_form.submit_button')
         resource.reload
         expect(resource.thumbnail_selection).to eq ThumbnailSelections::AUTO_GENERATED
-        expect(page).to have_selector('img[src="url.com/path/file"]')
       end
     end
   end
 
-  describe 'uploading thumbnail and selecting Use Uploaded Image' do
+  describe 'uploading thumbnail and selecting the "Use Uploaded Image" radio button' do
     before do
       visit Rails.application.routes.url_helpers.send("edit_dashboard_#{resource.class.to_s.downcase}_path", resource)
     end
 
-    it 'uploads a thumbnail and selects Use Uploaded Image radio button', js: true do
+    it 'uploads a thumbnail and selects "Use Uploaded Image" radio button', js: true do
       expect(page).to have_content('No Image').twice
       expect(page).to have_selector('input[id=thumbnail_form_thumbnail_selection_uploaded_image][disabled=disabled]')
       attach_file(Rails.root.join('spec', 'fixtures', 'image.png'))
@@ -55,12 +55,12 @@ RSpec.shared_examples 'a resource with thumbnail settings' do
     end
   end
 
-  describe 'selecting Use Default Icon' do
+  describe 'selecting "Use Default Icon" radio button' do
     before do
       visit Rails.application.routes.url_helpers.send("edit_dashboard_#{resource.class.to_s.downcase}_path", resource)
     end
 
-    it 'works from the Settings page' do
+    it 'updated thumbnail selection' do
       find('#thumbnail_form_thumbnail_selection_default_icon').set(true)
       click_button I18n.t!('dashboard.shared.thumbnail_form.submit_button')
       resource.reload
