1f4ebd18e4c9398b5f333189a788b57b27d0ecb1
Dann <whereismyjetpack@users.noreply.github.com>
2022-03-07 10:49:10 -0500

    background jobs database locking (#1269)
    In qa/prod when you upload a thumbnailable? file, the signature job, and the thumbnail job kick off at the same time, and the thumbnail job thomps (this is the technical term) on the file resource. we run both jobs with db table locking so that the thumbnail job will be in queue until the signature job is complete.

--- app/jobs/shrine/thumbnail_job.rb
+++ app/jobs/shrine/thumbnail_job.rb
@@ -4,9 +4,11 @@ class Shrine::ThumbnailJob < ApplicationJob
   queue_as :thumbnails
 
   def perform(record)
-    attacher = record.file_attacher
-    attacher.create_derivatives :thumbnail
-    record.save
+    record.with_lock do
+      attacher = record.file_attacher
+      attacher.create_derivatives :thumbnail
+      record.save
+    end
   rescue Shrine::AttachmentChanged, ActiveRecord::RecordNotFound
     # attachment has changed or record has been deleted, nothing to do
   end
