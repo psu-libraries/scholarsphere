48d467a768db7bf6cc76e593adff1cd80d324e5b
Nick Tiberi <nicktiberi@gmail.com>
2022-03-08 18:24:33 -0500

    OAI-PMH Endpoint (#1268)
    Exposes an OAI-PMH endpoint via the BlacklightOaiProvider gem.
    
    This endpoint allows us to expose metadata to external services
    such as Unpaywall. In addition to Dublin Core metadata fields,
    we also expose sets of documents based on their access level.

--- spec/models/solr_document_spec.rb
+++ spec/models/solr_document_spec.rb
@@ -3,7 +3,7 @@
 require 'rails_helper'
 
 RSpec.describe SolrDocument, type: :model do
-  subject { described_class.new(document) }
+  subject (:solr_document) { described_class.new(document) }
 
   describe '#deposited_at' do
     context 'when the value exists' do
@@ -37,6 +37,47 @@ RSpec.describe SolrDocument, type: :model do
         let(:document) { { thumbnail_selection_tesim: ThumbnailSelections::AUTO_GENERATED } }
 
         its(:default_thumbnail?) { is_expected.to be false }
+      end
+    end
+  end
+
+  describe '#to_semantic_values' do
+    let(:document) { { id: '123', identifier_tesim: ['user doi 1', 'user doi 2'] } }
+    let(:identifier) { solr_document.to_semantic_values[:identifier] }
+
+    context 'when the resource has a minted DOI' do
+      let(:document) { { id: '123', identifier_tesim: ['user doi 1', 'user doi 2'], all_dois_ssim: ['10.123.456'] } }
+
+      it 'has the correct identifiers' do
+        expect(identifier.count).to eq 4
+        expect(identifier).to eq [
+          "#{Rails.application.routes.url_helpers.root_url}resources/123",
+          'user doi 1',
+          'user doi 2',
+          '10.123.456'
+        ]
+      end
+    end
+
+    context 'when the resource does not have a minted DOI' do
+      it 'has the correct identifiers' do
+        expect(identifier.count).to eq 3
+        expect(identifier).to eq [
+          "#{Rails.application.routes.url_helpers.root_url}resources/123",
+          'user doi 1',
+          'user doi 2'
+        ]
+      end
+    end
+
+    context 'when the resource has no DOIs' do
+      let(:document) { { id: '123' } }
+
+      it 'has the correct identifiers' do
+        expect(identifier.count).to eq 1
+        expect(identifier).to eq [
+          "#{Rails.application.routes.url_helpers.root_url}resources/123"
+        ]
       end
     end
   end
