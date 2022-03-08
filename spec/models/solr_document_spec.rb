622c61a9ebf9179e3529210222cfaabe675f86fd
Alex Kiessling <ajkiessl@gmail.com>
2022-03-08 12:37:44 -0500

    solr doc return true for default_thumbnail? if cant find value in doc

--- spec/models/solr_document_spec.rb
+++ spec/models/solr_document_spec.rb
@@ -18,4 +18,26 @@ RSpec.describe SolrDocument, type: :model do
       its(:deposited_at) { is_expected.to be_nil }
     end
   end
+
+  describe '#default_thumbnail?' do
+    context 'when :thumbnail_selection_tesim cannot be found in the document' do
+      let(:document) { {} }
+
+      its(:default_thumbnail?) { is_expected.to be true }
+    end
+
+    context 'when :thumbnail_selection_tesim can be found in the document' do
+      context "when the value is #{ThumbnailSelections::DEFAULT_ICON}" do
+        let(:document) { { thumbnail_selection_tesim: ThumbnailSelections::DEFAULT_ICON } }
+
+        its(:default_thumbnail?) { is_expected.to be true }
+      end
+
+      context "when the value not #{ThumbnailSelections::DEFAULT_ICON}" do
+        let(:document) { { thumbnail_selection_tesim: ThumbnailSelections::AUTO_GENERATED } }
+
+        its(:default_thumbnail?) { is_expected.to be false }
+      end
+    end
+  end
 end
