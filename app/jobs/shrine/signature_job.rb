2423b3e45167c41b1aeb1aba8b9d8d243df7a0f5
whereismyjetpack <djb44@psu.edu>
2022-03-24 14:18:13 -0400

    download, process, delete files

--- app/jobs/shrine/signature_job.rb
+++ app/jobs/shrine/signature_job.rb
@@ -8,12 +8,14 @@ class Shrine::SignatureJob < ApplicationJob
     file_resource.with_lock do
       attacher = file_resource.file_attacher
       io = Shrine.uploaded_file(attacher.file_data)
+      tmp = io.download
       attacher.file.add_metadata(
-        'sha256' => Shrine.signature(io, :sha256),
-        'md5' => Shrine.signature(io, :md5)
+        'sha256' => Shrine.signature(tmp, :sha256),
+        'md5' => Shrine.signature(tmp, :md5)
       )
       attacher.write
       file_resource.save
+      tmp.delete
     end
   end
 end
