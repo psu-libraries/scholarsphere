05819cd30d7b58a7f1b4559ae4df9a9069dff1b7
whereismyjetpack <djb44@psu.edu>
2023-03-27 14:04:00 -0400

    fix syntax

--- app/javascript/frontend/scripts/view_statistics_chart.js
+++ app/javascript/frontend/scripts/view_statistics_chart.js
@@ -102,7 +102,7 @@ function drawChart (selection, data) {
   // Binary search function to look up closet data point to a given date
   const bisectDate = d3.bisector(row => row[0])
 
-  svg.on('mousemove click touchmove', (event) {
+  svg.on('mousemove click touchmove', (event) => {
     const mouseCoords = d3.pointer(event)
     const mouseDate = x.invert(mouseCoords[0])
     const [nearestDate,, nearestValue] = findNearestDataPoint(bisectDate, data, mouseDate)
