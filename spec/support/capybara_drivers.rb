23d8fcc2627e57e107e6c88de7b697b4e08028a3
Dann Bohn <djb44@psu.edu>
2023-10-24 10:06:31 -0400

    removes selenium remote

--- spec/support/capybara_drivers.rb
+++ spec/support/capybara_drivers.rb
@@ -1,28 +1,15 @@
 # frozen_string_literal: true
 
 RSpec.configure do |_config|
-  Capybara.javascript_driver = if ENV['SELENIUM']
-                                 :selenium_remote
-                               else
-                                 :selenium_chrome_headless
-                               end
+  Capybara.javascript_driver = :chrome_headless
 end
 
-Capybara.register_driver :selenium_remote do |app|
-  capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
-    chromeOptions: { args: [
-      'headless',
-      'no-sandbox',
-      'disable-gpu',
-      'window-size=1024,768',
-      'single-process'
-    ] }
+Capybara.register_driver :chrome_headless do |app| \
+  Capybara::Selenium::Driver.new(
+    app,
+    browser: :chrome,
+    options: Selenium::WebDriver::Chrome::Options.new(
+      args: %w[no-sandbox headless disable-gpu]
+    )
   )
-  Capybara.server_port = '3002'
-  Capybara.server_host = '0.0.0.0'
-  Capybara.app_host = "http://#{ENV['APP_HOST']}:#{Capybara.server_port}"
-  Capybara::Selenium::Driver.new(app,
-                                 browser: :remote,
-                                 desired_capabilities: capabilities,
-                                 url: ENV['SELENIUM_URL'].to_s)
 end
