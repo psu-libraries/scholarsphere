b995c24aac814e33c3c709c472f66ac7447c672f
Alex Kiessling <ajkiessl@gmail.com>
2024-02-05 15:33:12 -0500

    Integration with libanswers working via oath token.  Now need to tweak inputs to get the right data added to the ticket.

--- app/controllers/admin/contact_depositor_controller.rb
+++ app/controllers/admin/contact_depositor_controller.rb
@@ -4,7 +4,9 @@ module Admin
   class ContactDepositorController < ApplicationController
     def form
       resource = resource_klass.find(params[:id])
-      @contact_depositor_form = AdminContactDepositor.new(send_to_name: resource.depositor.display_name, send_to_email: resource.depositor.email)
+      @contact_depositor_form = AdminContactDepositor.new(send_to_name: resource.depositor.display_name, 
+                                                          send_to_email: resource.depositor.email, 
+                                                          cc_email_to: resource.edit_users.collect(&:email) << resource.depositor.email)
     end
 
     private
