febdda401f93a7f2339e582bec06628d7afd04a2
Dann Bohn <djb44@psu.edu>
2024-02-12 16:17:56 -0500

    '

--- spec/support/capybara_drivers.rb
+++ spec/support/capybara_drivers.rb
@@ -8,10 +8,10 @@ end
 Capybara.register_driver :selenium_chrome_headless do |app|
   options = ::Selenium::WebDriver::Chrome::Options.new
 
-  options.add_argument("--headless=new")
-  options.add_argument("--no-sandbox")
-  options.add_argument("--disable-dev-shm-usage")
-  options.add_argument("--window-size=1400,1400")
+  options.add_argument('--headless=new')
+  options.add_argument('--no-sandbox')
+  options.add_argument('--disable-dev-shm-usage')
+  options.add_argument('--window-size=1400,1400')
   options.add_argument('--disable-site-isolation-trials')
 
   Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
