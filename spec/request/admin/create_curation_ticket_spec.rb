8614e1a9ce0a10d4abb5ec0feb787c2c9e305c18
Alex Kiessling <ajkiessl@gmail.com>
2024-02-19 11:27:32 -0500

    Test for create curation ticket controller

---
+++ spec/request/admin/create_curation_ticket_spec.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'Application Settings', type: :request do
+  before { sign_in user }
+
+  describe 'GET /admin/works/:id/create_curation_ticket' do
+    let!(:work) { create(:work) }
+
+    context 'with a non-admin user' do
+      let(:user) { create(:user) }
+
+      specify do
+        expect { post admin_create_curation_ticket_url(work) }.to raise_error(ActionController::RoutingError)
+      end
+    end
+
+    context 'with an admin user' do
+      let(:user) { create(:user, :admin) }
+      let(:libanswer_response) { Struct.new(:admin_create_curation_ticket) }
+
+      before do
+        allow(LibanswersApiService).to receive(:new).and_return libanswer_response.new('Redirect Path')
+      end
+
+      specify do
+        post admin_create_curation_ticket_url(work)
+        expect(response).to redirect_to('Redirect Path')
+      end
+    end
+  end
+end
