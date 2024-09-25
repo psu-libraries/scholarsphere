0b722a4a21d4d70c6668bde7a93c9f8a48d62c04
Smulligan <smm5878@psu.edu>
2024-09-25 12:40:31 -0400

    yarn lint fix

--- app/javascript/controllers/work_type_controller.js
+++ app/javascript/controllers/work_type_controller.js
@@ -3,16 +3,16 @@ import { Controller } from 'stimulus'
 export default class extends Controller {
   static targets = ['workTypeSelect', 'helpText']
 
-  connect() {
-    this.toggleHelpText();
-    this.workTypeSelectTarget.addEventListener('change', () => this.toggleHelpText());
+  connect () {
+    this.toggleHelpText()
+    this.workTypeSelectTarget.addEventListener('change', () => this.toggleHelpText())
   }
 
-  toggleHelpText() {
+  toggleHelpText () {
     if (this.workTypeSelectTarget.value === 'instrument') {
-      this.helpTextTarget.style.display = 'block';
+      this.helpTextTarget.style.display = 'block'
     } else {
-      this.helpTextTarget.style.display = 'none';
+      this.helpTextTarget.style.display = 'none'
     }
   }
 }
