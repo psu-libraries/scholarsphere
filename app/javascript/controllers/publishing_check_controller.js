f70a3fba8a0777cec2f325bfb372a520d829b44c
Jesse Landis-Eigsti <jml8735@psu.edu>
2024-10-03 12:14:46 -0600

    1477 - adds the dialogue before the Curation Request
    Those submitting their dialogue for Curation also need to accept
    the depositor's agreement. To this, we reuse the modal from before,
    but with alternate text and button.

--- app/javascript/controllers/publishing_check_controller.js
+++ app/javascript/controllers/publishing_check_controller.js
@@ -1,14 +1,28 @@
 import { Controller } from 'stimulus'
 
 export default class extends Controller {
-  static targets = ['dialogBox', 'agreementCheckbox']
+  static targets = ['dialogBox', 'agreementCheckbox', 'publishButton', 'curationButton', 'publishText', 'curationText']
 
   connect () {
     this.agreementCheckboxTarget.removeAttribute('required');
   }
 
 
-  openDialog () {
+  openDialog(event) {
+    let dialogType = event.currentTarget.dataset.dialog
+    if (dialogType === 'publish') {
+      this.curationButtonTarget.classList.add('d-none');
+      this.curationTextTarget.classList.add('d-none');
+
+      this.publishButtonTarget.classList.remove('d-none');
+      this.publishTextTarget.classList.remove('d-none');
+    } else {
+      this.curationButtonTarget.classList.remove('d-none');
+      this.curationTextTarget.classList.remove('d-none');
+
+      this.publishButtonTarget.classList.add('d-none');
+      this.publishTextTarget.classList.add('d-none');
+    }
     this.dialogBoxTarget.showModal();
     // We only want to require the checkbox if the dialog is open.
     // If the checkbox is required when the dialog is closed, the Save and Exit button will not work.
