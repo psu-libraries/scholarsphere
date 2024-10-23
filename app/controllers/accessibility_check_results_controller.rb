33af4af1583cfd3d4e5f942212e5d650574f8d82
Smulligan <smm5878@psu.edu>
2024-10-23 11:55:55 -0400

    add ability to view formatted accessibility report

---
+++ app/controllers/accessibility_check_results_controller.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+class AccessibilityCheckResultsController < ApplicationController
+  def show
+    @result = AccessibilityCheckResult.find(params[:id])
+    @report = @result.readable_report
+    @title = @result.file_resource.file_data["metadata"]["filename"]
+  end
+end
