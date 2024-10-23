1c5a6571aed0fda03c4a41ffc033299978586ded
Smulligan <smm5878@psu.edu>
2024-10-23 12:39:56 -0400

    add authentication to ActionCable connection

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -2,5 +2,20 @@
 
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
+    identified_by :current_user
+
+    def connect
+      self.current_user = find_verified_user
+    end
+
+    private
+      def find_verified_user
+        session = Rails.env.production? ? "_session_id" : "_scholarsphere_session"
+        if verified_user = User.find_by(id: cookies.encrypted[session]["warden.user.user.key"][0][0])
+          verified_user
+        else
+          reject_unauthorized_connection
+        end
+      end
   end
 end
