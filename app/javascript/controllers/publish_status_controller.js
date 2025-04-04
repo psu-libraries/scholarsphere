d97c18a0df5a9ad15cfdd5e03f2378a19637cc21
Smulligan <smm5878@psu.edu>
2025-04-04 12:38:37 -0400

    yarn lint

--- app/javascript/controllers/publish_status_controller.js
+++ app/javascript/controllers/publish_status_controller.js
@@ -1,37 +1,37 @@
 import { Controller } from 'stimulus'
-import consumer from "../channels/consumer"
+import consumer from '../channels/consumer'
 
 export default class extends Controller {
   static targets = ['show_buttons', 'help_text'];
 
-  connect() {
+  connect () {
     consumer.subscriptions.create(
-      { channel: "PublishStatusChannel" },
+      { channel: 'PublishStatusChannel' },
       {
         connected: () => {
-            this.renderPublishStatus()
+          this.renderPublishStatus()
         },
         received: (data) => {
-            this.updatePublishStatus(data)
+          this.updatePublishStatus(data)
         }
       }
     )
-    console.log("TESTING: PublishStatusChannel subscription created");
+    console.log('TESTING: PublishStatusChannel subscription created')
   }
 
   updatePublishStatus (data) {
     this.data.set('allowPublish', data.allow_publish)
-    
+
     this.renderPublishStatus()
   }
 
   renderPublishStatus () {
     const primaryAction = this.data.get('primaryAction')
     const allowPublish = this.data.get('allowPublish') === 'true'
-    const show = allowPublish || primaryAction === "save_and_continue"
+    const show = allowPublish || primaryAction === 'save_and_continue'
 
-    console.log("TESTING: PublishStatusController show set as ", show);
-    this.show_buttonsTarget.classList.toggle('d-none', !show);
-    this.help_textTarget.classList.toggle('d-none', show);
+    console.log('TESTING: PublishStatusController show set as ', show)
+    this.show_buttonsTarget.classList.toggle('d-none', !show)
+    this.help_textTarget.classList.toggle('d-none', show)
   }
 }
