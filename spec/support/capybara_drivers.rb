cb8233a24f37f2fb55bc7d69b6f5ee93bb309a12
Jesse Landis-Eigsti <jml8735@psu.edu>
2025-04-25 14:27:50 -0600

    1694 - Change to capybara drivers

--- spec/support/capybara_drivers.rb
+++ spec/support/capybara_drivers.rb
@@ -1,12 +1,12 @@
 # frozen_string_literal: true
 
-RSpec.configure do |_config|
-  Capybara.javascript_driver = :firefox_headless
-end
-
 Capybara.register_driver :firefox_headless do |app|
   options = Selenium::WebDriver::Firefox::Options.new
   options.add_argument('--headless')
   options.add_argument('--window-size=1920,1080')
   Capybara::Selenium::Driver.new app, browser: :firefox, options: options
+end
+
+RSpec.configure do |_config|
+  Capybara.javascript_driver = :firefox_headless
 end
