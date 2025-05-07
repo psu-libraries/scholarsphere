4ab4342f4ca9a1d0e41bab1e95318147e46bcba5
Jesse Landis-Eigsti <jml8735@psu.edu>
2025-05-07 08:32:52 -0600

    1639 - Form error message component pulls focus
    Now, when a form error message appears, the screen reader will read it first.

---
+++ app/javascript/controllers/form_error_message_controller.js
@@ -0,0 +1,11 @@
+// @abstract Calls focus to the form error message
+
+import { Controller } from 'stimulus'
+
+export default class extends Controller {
+  connect () {
+    setTimeout(() => {
+      this.element.focus();
+    }, 100);
+  }
+}
