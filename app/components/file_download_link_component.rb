c1a2fed5bc0d154dab67e689b8cef0abdee3a5b3
Alex Kiessling <ajkiessl@gmail.com>
2025-05-22 16:09:13 -0400

    FileDownloadLinkComponent tests

--- app/components/file_download_link_component.rb
+++ app/components/file_download_link_component.rb
@@ -1,21 +1,21 @@
 # frozen_string_literal: true
 
 class FileDownloadLinkComponent < ViewComponent::Base
-  def initialize(file:, resource_id:)
-    @file = file
+  def initialize(file_version_membership:, resource_id:)
+    @file_version_membership = file_version_membership
     @resource_id = resource_id
   end
 
   def image?
-    @file.file_resource.image?
+    @file_version_membership.file_resource.image?
   end
 
   def alt_text
-    @file.file_resource.file.metadata['alt_text']
+    @file_version_membership.file_resource.file.metadata['alt_text']
   end
 
   def aria_label
-    download_text = t('dashboard.works.show.aria_download_file', file_name: @file.title)
+    download_text = t('dashboard.works.show.aria_download_file', file_name: @file_version_membership.title)
 
     return download_text unless image?
 
@@ -23,10 +23,10 @@ class FileDownloadLinkComponent < ViewComponent::Base
   end
 
   def download_path
-    Rails.application.routes.url_helpers.resource_download_path(@file.id, resource_id: @resource_id)
+    Rails.application.routes.url_helpers.resource_download_path(@file_version_membership.id, resource_id: @resource_id)
   end
 
   def download_title
-    I18n.t('resources.download', name: @file.title)
+    I18n.t('resources.download', name: @file_version_membership.title)
   end
 end
