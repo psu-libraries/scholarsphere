bae5ae0587f0dc4aaef65a592dddc9a29c839d33
Smulligan <smm5878@psu.edu>
2025-09-22 16:30:54 -0400

    refactor

--- app/models/file_resource.rb
+++ app/models/file_resource.rb
@@ -28,9 +28,14 @@ class FileResource < ApplicationRecord
   before_destroy :cannot_be_deleted_if_linked_to_thumbnail_upload
   after_commit :perform_update_index, on: [:create, :update]
 
+  scope :is_pdf , -> {
+    where("file_data->'metadata'->>'mime_type' = ?", PDF_MIME_TYPE)
+  }
+
   scope :needs_accessibility_check, -> {
     left_joins(:accessibility_check_result)
-      .where("file_data->'metadata'->>'mime_type' = ? AND accessibility_check_results.id IS NULL", PDF_MIME_TYPE)
+      .is_pdf
+      .where("accessibility_check_results.id IS NULL")
   }
 
   def self.reindex_all(relation: all, async: false)
