bf8a11a606c7959e446309edca00b672fa5bd580
Alex Kiessling <ajkiessl@gmail.com>
2025-09-29 15:35:54 -0400

    Refactored queries

--- app/models/file_resource.rb
+++ app/models/file_resource.rb
@@ -103,6 +103,14 @@ class FileResource < ApplicationRecord
     file.mime_type == PDF_MIME_TYPE
   end
 
+  def latest_remediation_work_version_candidate
+    work_versions.where.not(auto_remediation_started_at: nil).last
+  end
+
+  def latest_auto_remediated_work_version_after(version)
+    work_versions.where("id > ? AND auto_remediated_version = ?", version.id, true).last
+  end
+
   private
 
     def client
--- app/services/build_auto_remediated_work_version.rb
+++ app/services/build_auto_remediated_work_version.rb
@@ -2,29 +2,32 @@ require 'down'
 
 class BuildAutoRemediatedWorkVersion
   def self.call(file_resource, remediated_file_url)
-    version_being_remediated = file_resource.work_versions.where.not(work_versions: { auto_remediation_started_at: nil }).last
+    wv_being_remediated = file_resource.latest_remediation_work_version_candidate
 
     ActiveRecord::Base.transaction do
-      if file_resource.work_versions.where("work_versions.id > ? AND auto_remediated_version = ?", version_being_remediated.id, true).exists?
-        new_version = file_resource.work_versions.where(auto_remediated_version: true).last
-      else
-        new_version = BuildNewWorkVersion.call(version_being_remediated)
-        new_version.update auto_remediated_version: true
-      end
+      new_work_version = file_resource.latest_auto_remediated_work_version_after(wv_being_remediated) ||
+                    begin
+                      v = BuildNewWorkVersion.call(wv_being_remediated)
+                      v.update(auto_remediated_version: true)
+                      v
+                    end
+
       replacement_tempfile = Down.download(remediated_file_url)
-      version_file_link_to_delete = new_version.file_version_memberships.find_by(file_resource: file_resource)
-      version_file_link_to_delete.destroy!
-      new_version.file_resources.create!(
+      fvm_to_destroy = new_work_version.file_version_memberships
+                                       .find_by(file_resource: file_resource)
+      fvm_to_destroy.destroy! if fvm_to_destroy
+
+      new_work_version.file_resources.create!(
         file: replacement_tempfile,
         auto_remediated_version: true
       )
 
-      if new_version.file_resources.where('file_resources.remediation_job_uuid IS NOT NULL AND file_resources.auto_remediated_version = ?', false).exists?
-        new_version.save!
+      if new_work_version.has_remaining_auto_remediation_jobs?
+        new_work_version.save!
       else
-        new_version.publish!
+        new_work_version.publish!
       end
-      new_version
+      new_work_version
     end
   end
 end
