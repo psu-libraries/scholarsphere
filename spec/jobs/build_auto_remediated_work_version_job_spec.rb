1801aa6048e81228c9e67ac502ce140175a74f64
Jesse Landis-Eigsti <jml8735@psu.edu>
2025-10-23 13:30:58 -0600

    1806 - removes unused variables from spec

--- spec/jobs/build_auto_remediated_work_version_job_spec.rb
+++ spec/jobs/build_auto_remediated_work_version_job_spec.rb
@@ -6,15 +6,10 @@ RSpec.describe BuildAutoRemediatedWorkVersionJob do
   let(:file_resource) { create(:file_resource, remediation_job_uuid: existing_job_uuid) }
   let(:existing_job_uuid) { SecureRandom.uuid }
   let(:remediated_file_url) { 'https://example.com/remediated.pdf' }
-  let(:unpublished_work_version) { create(:work_version) }
-  let(:published_work_version) { create(:work_version, :published) }
-  let(:lib_answers) { LibanswersApiService.new }
 
   describe '#perform' do
     before do
-      allow(lib_answers).to receive(:admin_create_ticket)
       allow(BuildAutoRemediatedWorkVersion).to receive(:call)
-      allow(LibanswersApiService).to receive(:new).and_return(lib_answers)
     end
 
     context 'when the file resource with the given remediation_job_uuid does not exist' do
@@ -28,6 +23,7 @@ RSpec.describe BuildAutoRemediatedWorkVersionJob do
     context 'when the file resource with the given remediation_job_uuid exists' do
       it 'calls the service when perform_now is used' do
         described_class.perform_now(file_resource.remediation_job_uuid, remediated_file_url)
+
         expect(BuildAutoRemediatedWorkVersion).to have_received(:call).with(file_resource, remediated_file_url)
       end
     end
