3c3d20411245b93a51f507c0883cb40f00a10f36
Alex Kiessling <ajkiessl@gmail.com>
2025-10-31 13:37:17 -0400

    Check rails environment in #able_to_auto_remediate?.  If prodcution return false.  This turns off the remediation feature in production.

--- app/services/auto_remediate_service.rb
+++ app/services/auto_remediate_service.rb
@@ -18,6 +18,10 @@ class AutoRemediateService
   end
 
   def able_to_auto_remediate?
+    # The following line can be removed when
+    # auto-remediation is enabled in production
+    return false if Rails.env.production?
+
     work_version.latest_published_version? &&
       work_version.auto_remediation_started_at.nil? &&
       !work_version.auto_remediated_version &&
