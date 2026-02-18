24e5aafb874e9a1a9b8ac1aca93e05c88ef6bed0
Alex Kiessling <ajkiessl@gmail.com>
2026-02-18 16:23:11 -0500

    Niftany

--- spec/jobs/shrine/thumbnail_job_spec.rb
+++ spec/jobs/shrine/thumbnail_job_spec.rb
@@ -26,8 +26,7 @@ RSpec.describe Shrine::ThumbnailJob, skip: !ci_build? do
     it 'reindexes associated work versions when no thumbnail upload is present' do
       work_version_one = instance_spy(WorkVersion)
       work_version_two = instance_spy(WorkVersion)
-      allow(pdf_record).to receive(:work_versions).and_return([work_version_one, work_version_two])
-      allow(pdf_record).to receive(:thumbnail_upload).and_return(nil)
+      allow(pdf_record).to receive_messages(work_versions: [work_version_one, work_version_two], thumbnail_upload: nil)
 
       described_class.perform_now(pdf_record)
 
@@ -39,8 +38,7 @@ RSpec.describe Shrine::ThumbnailJob, skip: !ci_build? do
       thumbnail_upload = create(:thumbnail_upload)
       resource = thumbnail_upload.resource
       allow(resource).to receive(:update_index)
-      allow(pdf_record).to receive(:work_versions).and_return([])
-      allow(pdf_record).to receive(:thumbnail_upload).and_return(thumbnail_upload)
+      allow(pdf_record).to receive_messages(work_versions: [], thumbnail_upload: thumbnail_upload)
 
       described_class.perform_now(pdf_record)
 
