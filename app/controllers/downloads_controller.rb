f90ff3711b92339a2f9a546ad2d41031037aa94c
Alex Kiessling <32677188+ajkiessl@users.noreply.github.com>
2026-04-10 12:08:44 -0400

    Token verification on download links (#1936)
    * Replace download=true process on file download links/controller with a token verification process.  I set the token to expire after 1 minute, this may need adjusted.  This will confirm that users are downlaoding and triggering remediation only when the show page has been rendered in a browser.
    
    * Bump the expire time to 8 minutes

--- app/controllers/downloads_controller.rb
+++ app/controllers/downloads_controller.rb
@@ -8,6 +8,8 @@ class DownloadsController < ApplicationController
   def content
     work_version = WorkVersion.find_by!(uuid: download_params[:resource_id])
     file_version = work_version.file_version_memberships.find(download_params[:id])
+    download_requested = valid_download_token?(download_params[:download_token], file_version.id)
+
     authorize(file_version)
     file_version.file_resource.count_view! if count_view?(file_version.file_resource)
 
@@ -16,9 +18,9 @@ class DownloadsController < ApplicationController
       current_user,
       file_version.file_resource.can_remediate?
     )
-    remediation_service.call if remediation_service.able_to_auto_remediate? && download_params[:download]
+    remediation_service.call if remediation_service.able_to_auto_remediate? && download_requested
 
-    redirect_to s3_presigned_url(file_version, download: download_params[:download]), allow_other_host: true
+    redirect_to s3_presigned_url(file_version, download: download_requested), allow_other_host: true
   end
 
   private
@@ -37,8 +39,20 @@ class DownloadsController < ApplicationController
       SessionViewStatsCache.call(session: session, resource: resource)
     end
 
+    def valid_download_token?(token, file_version_id)
+      return false if token.blank?
+
+      token_file_version_id = download_token_verifier.verify(token, purpose: :download_request)
+      token_file_version_id.to_i == file_version_id.to_i
+    rescue ActiveSupport::MessageVerifier::InvalidSignature
+      false
+    end
+
+    def download_token_verifier
+      Rails.application.message_verifier(:download_request_token)
+    end
+
     def download_params
-      params[:download] = ActiveModel::Type::Boolean.new.cast(params[:download])
-      params.permit(:id, :resource_id, :download)
+      params.permit(:id, :resource_id, :download_token)
     end
 end
