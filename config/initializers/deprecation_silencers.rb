ebdf8b059d900418db6649586ea4b5459d5576aa
Alex Kiessling <ajkiessl@gmail.com>
2026-04-29 16:17:33 -0400

    Remove deprecation silencer.  This is managed within rails now in the production environment file

--- config/initializers/deprecation_silencers.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-# Silence deprecation warnings made in the deprecation gem
-
-if ENV['RAILS_ENV'] == 'production'
-  Deprecation.default_deprecation_behavior = :silence
-end
