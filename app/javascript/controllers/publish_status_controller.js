5776a383533fd86586bf46aeb466f58b4286f4bf
Smulligan <smm5878@psu.edu>
2026-05-18 14:54:40 -0400

    block publish for version mismatch on initial page load and unblock publish for non oa works

--- app/javascript/controllers/publish_status_controller.js
+++ app/javascript/controllers/publish_status_controller.js
@@ -4,46 +4,51 @@ import consumer from '../channels/consumer'
 export default class extends Controller {
   static targets = ['show_buttons', 'help_text'];
 
-  connect () {
-    this.data.set('versionAllowed', 'true')
+connect () {
+  document.addEventListener('open-access:version-updated', this.handleVersionUpdate)
 
-    consumer.subscriptions.create(
-      { channel: 'PublishStatusChannel' },
-      {
-        connected: () => {
-          this.renderPublishStatus()
-        },
-        received: (data) => {
-          this.updatePublishStatus(data)
-        }
+  consumer.subscriptions.create(
+    { channel: 'PublishStatusChannel' },
+    {
+      connected: () => {
+        this.renderPublishStatus()
+      },
+      received: (data) => {
+        this.updatePublishStatus(data)
       }
-    )
-  }
+    }
+  )
+}
 
   updatePublishStatus (data) {
-    this.data.set('accessibilityAllowed', data.allow_publish)
+    this.data.set('allowPublish', data.allow_publish)
+
     this.renderPublishStatus()
   }
 
   renderPublishStatus () {
     const primaryAction = this.data.get('primaryAction')
-
-    const accessibilityAllowed = this.data.get('accessibilityAllowed') === 'true'
-    const versionAllowed = this.data.get('versionAllowed') === 'true'
-
-    const allowPublish = accessibilityAllowed && versionAllowed
-    const show = allowPublish || primaryAction === 'save_and_continue'
-
+    const allowPublish = this.data.get('allowPublish') === 'true'
+    const versionAllowed = this.data.get('versionAllowed') || 'true'
+    const versionAllowsPublish = versionAllowed === 'true'
+    const show = (allowPublish && versionAllowsPublish) || primaryAction === 'save_and_continue'
     this.show_buttonsTarget.classList.toggle('d-none', !show)
     this.help_textTarget.classList.toggle('d-none', show)
-
      if (!show) {
-      const blockReason = !accessibilityAllowed ? 'accessibility' : 'version'
-
+      const blockReason = !allowPublish ? 'accessibility' : 'version'
       this.help_textTarget.textContent =
         blockReason === 'version'
           ? this.data.get('versionMessage')
           : this.data.get('accessibilityMessage')
     }
   }
+
+  disconnect() {
+  document.removeEventListener('open-access:version-updated', this.handleVersionUpdate)
+}
+
+handleVersionUpdate = (event) => {
+  this.data.set('versionAllowed', event.detail.versionAllowed)
+  this.renderPublishStatus()
+}
 }
