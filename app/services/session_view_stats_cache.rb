98876304219eaa921403c2d98fde02321dc23b6a
Alex Kiessling <ajkiessl@gmail.com>
2026-05-22 16:22:35 -0400

    Configure key based ttl for seesion view cache

--- app/services/session_view_stats_cache.rb
+++ app/services/session_view_stats_cache.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class SessionViewStatsCache
+  DEFAULT_TTL = 24.hours.to_i
+
   def self.call(session:, resource:)
     redis = Redis.new(Rails.configuration.redis)
     digest = Digest::MD5.hexdigest("#{session.id}#{resource.class.name}#{resource.id}")[0..6]
@@ -9,8 +11,14 @@ class SessionViewStatsCache
     return false if redis.get(key)
 
     redis.set(key, true)
-    redis.expire(key, Rails.configuration.redis[:ttl])
+    redis.expire(key, ttl)
 
     true
   end
+
+  def self.ttl
+    Integer(ENV.fetch('SESSION_VIEW_STATS_CACHE_TTL', DEFAULT_TTL), exception: false) || DEFAULT_TTL
+  end
+
+  private_class_method :ttl
 end
