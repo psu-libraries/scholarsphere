9e3a7a4121375d22c091ee44e801972dbda809ef
Alex Kiessling <ajkiessl@gmail.com>
2026-06-04 14:27:18 -0400

    Adds test for resetting open access version on file upload.

--- spec/request/dashboard/form/files_controller_spec.rb
+++ spec/request/dashboard/form/files_controller_spec.rb
@@ -4,7 +4,14 @@ require 'rails_helper'
 
 RSpec.describe Dashboard::Form::FilesController, type: :request do
   describe 'PATCH /dashboard/form/work_versions/:id/files' do
-    let(:work_version) { create(:work_version, :draft, open_access: open_access) }
+    let(:work_version) do
+      create(
+        :work_version,
+        :draft,
+        open_access: open_access,
+        open_access_version: OpenAccessVersion::VersionValues::ACCEPTED
+      )
+    end
     let(:user) { work_version.depositor.user }
     let(:open_access) { true }
     let(:request_params) { { work_version: { id: work_version.id } } }
@@ -17,12 +24,13 @@ RSpec.describe Dashboard::Form::FilesController, type: :request do
     context 'when open access is enabled' do
       let(:open_access) { true }
 
-      it 'enqueues open access version guessing' do
+      it 'enqueues open access version guessing and resets open_access_version' do
         patch dashboard_form_files_path(work_version), params: request_params
 
         expect(response).to have_http_status(:redirect)
         expect(response).to redirect_to(dashboard_form_publish_path(work_version))
         expect(OpenAccessVersionGuesserJob).to have_received(:perform_later).with(work_version.id)
+        expect(work_version.reload.open_access_version).to be_nil
       end
     end
 
@@ -35,6 +43,7 @@ RSpec.describe Dashboard::Form::FilesController, type: :request do
         expect(response).to have_http_status(:redirect)
         expect(response).to redirect_to(dashboard_form_publish_path(work_version))
         expect(OpenAccessVersionGuesserJob).not_to have_received(:perform_later)
+        expect(work_version.reload.open_access_version).to eq(OpenAccessVersion::VersionValues::ACCEPTED)
       end
     end
   end
