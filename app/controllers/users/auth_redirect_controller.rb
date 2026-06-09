ef8dc4ce23f63175d486a9bc05d574a334a06395
Alex Kiessling <ajkiessl@gmail.com>
2026-06-09 16:00:08 -0400

    If user is not logged in, and trying to access the file edit path they get redirected to an azure login hacky screen that clicks Login via javascript.  This alos passes along the return url, so the user is sent back to the file edit page.  This is a bit hacky, and we should investigate actual autologin for the future, but this works for now.

---
+++ app/controllers/users/auth_redirect_controller.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+class Users::AuthRedirectController < ApplicationController
+  def show
+    return_to = params[:return_to].presence
+    store_location_for(:user, return_to) if return_to
+  end
+end
---
+++ spec/request/users/auth_redirect_controller_spec.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Users::AuthRedirectController, type: :request do
+  describe 'GET /users/auth/azure_oauth/redirect' do
+    it 'stores return_to in session when provided' do
+      return_to = '/dashboard/form/work_versions/123/files'
+
+      get user_azure_oauth_redirect_path(return_to: return_to)
+
+      expect(response).to have_http_status(:ok)
+      expect(session['user_return_to']).to eq(return_to)
+    end
+  end
+end
