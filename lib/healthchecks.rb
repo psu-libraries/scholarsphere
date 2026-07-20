adff751137d0419828b4e532060a5aab554191d7
Alex Kiessling <ajkiessl@gmail.com>
2026-07-20 15:34:09 -0400

    Naming convention fix

--- lib/healthchecks.rb
+++ lib/healthchecks.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-module Healthchecks
+module HealthChecks
   require 'healthchecks/queue_dead_set_check'
   require 'healthchecks/queue_latency_check'
   require 'healthchecks/version_check'
