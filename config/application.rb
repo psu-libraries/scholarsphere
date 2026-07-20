0176b8053aad056b4403f67ba8a4d482545be596
Alex Kiessling <ajkiessl@gmail.com>
2026-07-20 14:42:15 -0400

    Run rails installer and try to merge in rails 8 changes

--- config/application.rb
+++ config/application.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 require_relative 'boot'
 
 require 'rails'
@@ -14,8 +12,7 @@ require 'action_mailbox/engine'
 require 'action_text/engine'
 require 'action_view/railtie'
 require 'action_cable/engine'
-require 'sprockets/railtie'
-require 'view_component/engine'
+# require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -34,29 +31,37 @@ module Scholarsphere
       ENV['READ_ONLY'] == 'true'
     end
 
-    config.active_record.yaml_column_permitted_classes = [ActiveSupport::HashWithIndifferentAccess]
-
-    config.generators { |generator| generator.test_framework :rspec }
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
+
+    # Please, add to the `ignore` list any other `lib` subdirectories that do
+    # not contain `.rb` files, or that should not be reloaded or eager loaded.
+    # Common ones are `templates`, `generators`, or `middleware`, for example.
+    config.autoload_lib(ignore: %w[assets tasks])
+
+    # Configuration for the application, engines, and railties goes here.
+    #
+    # These settings can be overridden in specific environments using the files
+    # in config/environments, which are processed later.
+    #
+    # config.time_zone = "Central Time (US & Canada)"
+    # config.eager_load_paths << Rails.root.join("extras")
+
+    # Don't generate system test files.
+    config.generators.system_tests = nil
+
+    # ActiveJob config
+    config.active_job.queue_adapter = :sidekiq
 
     config.admin_group = ENV.fetch('ADMIN_GROUP', 'umg-up.dlt.scholarsphere-admin')
     config.viewer_group = ENV.fetch('VIEWER_GROUP', 'umg-up.dlt.scholarsphere-viewer')
     config.psu_affiliated_group = ENV.fetch('PSU_AFFILIATED_GROUP', 'psu-affiliated-group')
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
+    config.active_record.yaml_column_permitted_classes = [ActiveSupport::HashWithIndifferentAccess]
+    config.generators { |generator| generator.test_framework :rspec }
 
     config.redis = Scholarsphere::RedisConfig.new.to_hash
     config.solr = Scholarsphere::SolrConfig.new
-
-    # ActiveJob config
-    config.active_job.queue_adapter = :sidekiq
-
-    # Don't generate system test files.
-    config.generators.system_tests = nil
 
     config.time_zone = 'Eastern Time (US & Canada)'
 
