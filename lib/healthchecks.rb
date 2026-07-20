28db2cee5e859f24b0ef3a1e1feaf2947e1273dd
Alex Kiessling <ajkiessl@gmail.com>
2026-07-20 15:41:08 -0400

    Cover all 'Healthchecks' invocations

--- lib/healthchecks.rb
+++ lib/healthchecks.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-module HealthChecks
+module Healthchecks
   require 'healthchecks/queue_dead_set_check'
   require 'healthchecks/queue_latency_check'
   require 'healthchecks/version_check'
--- lib/healthchecks/queue_latency_check.rb
+++ lib/healthchecks/queue_latency_check.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-module HealthChecks
+module Healthchecks
   class QueueLatencyCheck < OkComputer::Check
     attr_reader :threshold
 
