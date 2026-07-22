d9b926d8591e6c907927c19c27eef0712ee4b2b7
Alex Kiessling <ajkiessl@gmail.com>
2026-07-22 12:41:07 -0400

    Revert controller params syntax to old non-rails 8 syntax for controllers that were failing controller tests

--- app/controllers/dashboard/form/contributors_controller.rb
+++ app/controllers/dashboard/form/contributors_controller.rb
@@ -22,29 +22,28 @@ module Dashboard
 
         def resource_params
           params
-            .expect(
-              param_key => [:owner,
-                            :manufacturer,
-                            contributor: [],
-                            creators_attributes: [
-                              :id,
-                              :actor_id,
-                              :_destroy,
-                              :display_name,
-                              :position,
-                              :given_name,
-                              :surname,
-                              :email,
-                              actor_attributes: [
-                                :id,
-                                :email,
-                                :given_name,
-                                :surname,
-                                :psu_id,
-                                :orcid
-                              ]
-                            ]]
-            )
+            .require(param_key)
+            .permit(:owner,
+                    :manufacturer,
+                    contributor: [],
+                    creators_attributes: [
+                      :id,
+                      :actor_id,
+                      :_destroy,
+                      :display_name,
+                      :position,
+                      :given_name,
+                      :surname,
+                      :email,
+                      actor_attributes: [
+                        :id,
+                        :email,
+                        :given_name,
+                        :surname,
+                        :psu_id,
+                        :orcid
+                      ]
+                    ])
         end
 
         def next_page_path
