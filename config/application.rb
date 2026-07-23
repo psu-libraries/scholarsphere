4aa12b51255c317320564d5e37d4e9fa82fac19c
Alex Kiessling <ajkiessl@gmail.com>
2026-07-23 15:44:28 -0400

    Bump load_defaults to 8.1.  Fix date parsing error with ViewStatistics.  My understanding is that with newer versions of rails, ActiveRecord will pull date fields in postgres as ruby Date objects.  So, they dont need to be parsed like strings

--- config/application.rb
+++ config/application.rb
@@ -33,8 +33,7 @@ module Scholarsphere
       ENV['READ_ONLY'] == 'true'
     end
 
-    # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 7.0
+    config.load_defaults 8.1
 
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
