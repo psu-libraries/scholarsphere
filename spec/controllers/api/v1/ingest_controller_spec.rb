d717e7f7d0ee99ebe479f56cb12e3af27c7eefa5
Alex Kiessling <ajkiessl@gmail.com>
2026-07-23 12:26:23 -0400

    Fixed some tests

--- spec/controllers/api/v1/ingest_controller_spec.rb
+++ spec/controllers/api/v1/ingest_controller_spec.rb
@@ -164,7 +164,7 @@ RSpec.describe Api::V1::IngestController do
       it 'reports the error with the missing parameter' do
         expect(response).to be_bad_request
         expect(json_response['message']).to eq('Bad request')
-        expect(json_response['errors']).to include(/param is missing or the value is empty: depositor/)
+        expect(json_response['errors']).to include(/param is missing or the value is empty or invalid: depositor/)
       end
     end
 
--- spec/mailers/auto_remediation_mailer_spec.rb
+++ spec/mailers/auto_remediation_mailer_spec.rb
@@ -15,8 +15,12 @@ RSpec.describe AutoRemediationMailer do
       it 'renders the remediated_version_created template' do
         expect(mail.subject).to eq('Accessible Version of Your Work Now Available on ScholarSphere')
         expect(mail.to).to contain_exactly('contributor@example.com')
+        resource_url = Rails.application.routes.url_helpers.resource_url(
+          params[:work_version_uuid],
+          host: Rails.configuration.action_mailer.default_url_options[:host]
+        )
         expect(mail.body.encoded).to include(
-          "<a href=\"#{Rails.application.routes.url_helpers.root_url}resources/123e4567-e89b-12d3-a456-426614174000\">Title of Work</a>"
+          "<a href=\"#{resource_url}\">Title of Work</a>"
         )
         expect(mail.body.encoded).to include(
           'The Libraries’ Adaptive Technology and Services team will manually '
