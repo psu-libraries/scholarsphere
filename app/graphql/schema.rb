6cdf14e4fe39c6caf082823066454a5e5fd6de23
Alex Kiessling <32677188+ajkiessl@users.noreply.github.com>
2026-08-14 15:45:14 -0400

    Security Vulnerabilities and remove GraphQL (#2074)
    * Upgrade @redocly/cli and graphiql
    
    * Removes all GraphQL code and graphiql code
    
    * Remove public controller graphql query tests
    
    * Upgrade ws, postcss, nanoid, and some uppy packages to clear up some security vulns
    
    * Clean up remaining yarn vulns.  Removed a handful or yarn packages that had vulnerabilities that are not used

--- app/graphql/schema.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-
-class Schema < GraphQL::Schema
-  query Types::Query
-end
--- app/graphql/types/uuid.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-module Types
-  class Uuid < GraphQL::Types::ID
-    description 'Unique identifier for a resource in Scholarsphere'
-  end
-end
